7a9ce6bec7e536a6f58df7947add914778f29cbd
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-07-06 16:03:18 -0300

    atualizações na regra de soma da nota com a recuperação

--- View/rendimento.php
+++ View/rendimento.php
@@ -740,12 +740,34 @@ periodo_id=1 and aluno_id=$idaluno  group by avaliacao,periodo_id ");
 
 
 $nota_tri_1=0;
+$nota_av3_1='';
+$nota_rp_1='';
+
 foreach ($result_nota_aula1 as $key => $value) {
 
   if ($value['avaliacao']!='RP') {
     $nota_tri_1+=$value['nota'];
+
+
   }
+// ***************************************
+  if ($value['avaliacao']=='av3') {
+    $nota_av3_1=$value['nota'];
+
+  }
+
+  if ($value['avaliacao']=='RP') {
+    $nota_rp_1=$value['nota'];
+   
+
+  }
+
 }
+
+if ($nota_tri_1<5 && $nota_rp_1!='' && $nota_rp_1>$nota_av3_1) {
+ $nota_tri_1=($nota_tri_1-$nota_av3_1)+$nota_rp_1;
+}
+
 echo "$nota_tri_1";
 ?>
     </o:p></span></p>
@@ -798,11 +820,27 @@ periodo_id=2 and aluno_id=$idaluno  group by avaliacao,periodo_id ");
 
 
 $nota_tri_2=0;
+$nota_av3_2='';
+$nota_rp_2='';
 foreach ($result_nota_aula2 as $key => $value) {
   if ($value['avaliacao']!='RP') {
     $nota_tri_2+=$value['nota'];
   }
+// ***************************************
+  if ($value['avaliacao']=='av3') {
+    $nota_av3_2=$value['nota'];
+  }
+
+  if ($value['avaliacao']=='RP') {
+    $nota_rp_2=$value['nota'];
+  }
+
 }
+
+if ($nota_tri_2<5 && $nota_rp_2!='' && $nota_rp_2>$nota_av3_2) {
+ $nota_tri_2=($nota_tri_2-$nota_av3_2)+$nota_rp_2;
+}
+
 echo "$nota_tri_2";
 ?>
 
@@ -857,11 +895,28 @@ periodo_id=3 and aluno_id=$idaluno  group by avaliacao,periodo_id ");
 
 
 $nota_tri_3=0;
+$nota_av3_3='';
+$nota_rp_3='';
+
 foreach ($result_nota_aula2 as $key => $value) {
   if ($value['avaliacao']!='RP') {
      $nota_tri_3+=$value['nota'];
   }
+// ***************************************
+  if ($value['avaliacao']=='av3') {
+    $nota_av3_3=$value['nota'];
+  }
+
+  if ($value['avaliacao']=='RP') {
+    $nota_rp_3=$value['nota'];
+  }
+
 }
+
+if ($nota_tri_3<5 && $nota_rp_3!='' && $nota_rp_3>$nota_av3_3) {
+ $nota_tri_3=($nota_tri_3-$nota_av3_3)+$nota_rp_3;
+}
+
 echo "$nota_tri_3";
 ?>
     <o:p></o:p></span></p>
