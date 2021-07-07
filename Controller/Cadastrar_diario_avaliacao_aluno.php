f37a06addbfd466fc36be6a858ef3e3b2a712b22
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-07-07 16:05:26 -0300

    colocando em pratica avaliação

--- Controller/Cadastrar_diario_avaliacao_aluno.php
+++ Controller/Cadastrar_diario_avaliacao_aluno.php
@@ -21,67 +21,60 @@ try {
       $nota=0;
 
     $url_get=$_POST['url_get'];
-    echo"$url_get <br>";
-
-//$conteudo_aula_id= $conexao->lastInsertId();
 
 
-// foreach ($_POST['aluno_id'] as $key => $value) {
-//       $aluno_id=$_POST['aluno_id'][$key];
+foreach ($_POST['aluno_id'] as $key => $value) {
+      $aluno_id=$_POST['aluno_id'][$key];
 
-//     limpa_nota_diario($conexao,$idescola,$idturma,$iddisciplina,$aluno_id,$periodo,$data,$avaliacao);
+    limpa_nota_diario($conexao,$idescola,$idturma,$iddisciplina,$aluno_id,$periodo,$data,$avaliacao);
 
-//       $parecer_descritivo='';
-//       if (isset($_POST["parecer_descritivo$aluno_id"])) {
-//         $parecer_descritivo=$_POST["parecer_descritivo$aluno_id"];
-//       }
+      $parecer_descritivo='';
+      if (isset($_POST["parecer_descritivo$aluno_id"])) {
+        $parecer_descritivo=$_POST["parecer_descritivo$aluno_id"];
+      }
       
-//       $nota=0;
-//       if (isset($_POST["nota$aluno_id"])) {
-//         if ($_POST["nota$aluno_id"] !="") {
-//             $nota=trim($_POST["nota$aluno_id"]);
-//             $nota=str_replace(',','.',$nota);
-//         }else{
+      $nota=0;
+      if (isset($_POST["nota$aluno_id"])) {
+        if ($_POST["nota$aluno_id"] !="") {
+            $nota=trim($_POST["nota$aluno_id"]);
+            $nota=str_replace(',','.',$nota);
+        }else{
 
-//         }
-//       }
+        }
+      }
 
-//       if (isset($_POST["parecer_sigla$aluno_id"])) {
+      if (isset($_POST["parecer_sigla$aluno_id"])) {
          
-//           foreach ($_POST["parecer_sigla$aluno_id"] as $key => $value) {
-//               $sigla=null;
-//               if (isset($_POST["parecer_sigla$aluno_id"][$key])) {
-//                 $sigla=$_POST["parecer_sigla$aluno_id"][$key];
-//               }      
+          foreach ($_POST["parecer_sigla$aluno_id"] as $key => $value) {
+              $sigla=null;
+              if (isset($_POST["parecer_sigla$aluno_id"][$key])) {
+                $sigla=$_POST["parecer_sigla$aluno_id"][$key];
+              }      
 
-//               $parecer_disciplina_id=0;
-//               if (isset($_POST["descricao_parecer$aluno_id"][$key])) {
-//                 $parecer_disciplina_id=$_POST["descricao_parecer$aluno_id"][$key];
-//               }
+              $parecer_disciplina_id=0;
+              if (isset($_POST["descricao_parecer$aluno_id"][$key])) {
+                $parecer_disciplina_id=$_POST["descricao_parecer$aluno_id"][$key];
+              }
 
-//     limpa_parecer_nota_diario($conexao,$idescola,$idturma,$iddisciplina,$aluno_id,$periodo,$data,$parecer_disciplina_id,$avaliacao);
+    limpa_parecer_nota_diario($conexao,$idescola,$idturma,$iddisciplina,$aluno_id,$periodo,$data,$parecer_disciplina_id,$avaliacao);
 
-//               cadastro_nota($conexao,$nota, 
-//                 $parecer_disciplina_id, $parecer_descritivo, $sigla,$idescola, $idturma, $iddisciplina, $aluno_id, $periodo, $data,$avaliacao);
-//           }
+              cadastro_nota($conexao,$nota, 
+                $parecer_disciplina_id, $parecer_descritivo, $sigla,$idescola, $idturma, $iddisciplina, $aluno_id, $periodo, $data,$avaliacao);
+          }
 
-//     }else{
-//         cadastro_nota($conexao,$nota, 
-//                 $parecer_disciplina_id, $parecer_descritivo, $sigla,$idescola, $idturma, $iddisciplina, $aluno_id, $periodo, $data,$avaliacao);
-//     }
+    }else{
+        cadastro_nota($conexao,$nota, 
+                $parecer_disciplina_id, $parecer_descritivo, $sigla,$idescola, $idturma, $iddisciplina, $aluno_id, $periodo, $data,$avaliacao);
+    }
 
     
-// }
-echo "e: $idescola,t: $idturma,d: $iddisciplina,a: $aluno_id,p: $periodo,d: $data,a: $avaliacao <br>";
+}
 
     $_SESSION['status']=1;
-    //header("location: ../View/diario_avaliacao.php?$url_get");
+    header("location: ../View/diario_avaliacao.php?$url_get");
 } catch (Exception $e) {
-
-echo "<h1>sistema está em manuteção previsão de normalização 17:00 turma: $idturma</h1><br>
-$e";
-    $_SESSION['status']=0;
-   //header("location: ../View/diario_avaliacao.php?$url_get");;
+   $_SESSION['status']=0;
+   header("location: ../View/diario_avaliacao.php?$url_get");;
 
 }
 ?>
