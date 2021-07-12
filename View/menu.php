5187114a2067456585293ab02684a6e4a57aa2d8
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-07-12 19:12:10 -0300

    ajuste visual nome funcao

--- View/menu.php
+++ View/menu.php
@@ -22,7 +22,7 @@
           </a>
           <?php 
             if (isset($_SESSION['cargo'])) {
-              echo "<b class='text-danger'>".$_SESSION['cargo']."</b>";
+              echo "<b class='text-danger'>".$_SESSION['cargo']." (a)</b>";
             }
           ?>
         </div>
