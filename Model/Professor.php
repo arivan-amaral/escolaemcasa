7246187da194206c7afd9a4790051d28ec528278
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-07-26 10:54:44 -0300

    ajuste pesquisa professor tava vindo secretario

--- Model/Professor.php
+++ Model/Professor.php
@@ -32,7 +32,7 @@
 	}
 
 	function pesquisar_professor_associacao($conexao,$pesquisa) {
-    	$result=$conexao->query("SELECT * FROM funcionario WHERE descricao_funcao !='Coordenador' AND descricao_funcao !='Coordenadora' and nome like '%$pesquisa%'  and funcionario.status=1 ");
+    	$result=$conexao->query("SELECT * FROM funcionario WHERE descricao_funcao !='Coordenador' and  descricao_funcao !='Secretário' AND descricao_funcao !='Coordenadora' and nome like '%$pesquisa%'  and funcionario.status=1 ");
     	return $result ;
 	}
 
