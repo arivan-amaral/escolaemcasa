c42c0b883e3284cbc505be8d60683bee2727788e
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-08-01 12:16:57 -0300

    olho magico mostrar senha

--- View/rodape.php
+++ View/rodape.php
@@ -89,7 +89,31 @@
                   <div class="input-group">
                       <label>Senha</label>
                   </div>
-                    <input type="password" class="form-control"  name="senha" required>
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+                    <input type="password" class="form-control"  name="senha" id='senha' required>
+                    <a id="olho" href="#olho" name="olho">MOSTRAR SENHA </a>
+                    <br>
+                    <!-- <img id="olho" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABDUlEQVQ4jd2SvW3DMBBGbwQVKlyo4BGC4FKFS4+TATKCNxAggkeoSpHSRQbwAB7AA7hQoUKFLH6E2qQQHfgHdpo0yQHX8T3exyPR/ytlQ8kOhgV7FvSx9+xglA3lM3DBgh0LPn/onbJhcQ0bv2SHlgVgQa/suFHVkCg7bm5gzB2OyvjlDFdDcoa19etZMN8Qp7oUDPEM2KFV1ZAQO2zPMBERO7Ra4JQNpRa4K4FDS0R0IdneCbQLb4/zh/c7QdH4NL40tPXrovFpjHQr6PJ6yr5hQV80PiUiIm1OKxZ0LICS8TWvpyyOf2DBQQtcXk8Zi3+JcKfNafVsjZ0WfGgJlZZQxZjdwzX+ykf6u/UF0Fwo5Apfcq8AAAAASUVORK5CYII="/>  -->
+                    <script type="text/javascript">
+                      
+                      var senha = $('#senha');
+                      var olho= $("#olho");
+
+                      olho.mousedown(function() {
+                        senha.attr("type", "text");
+                      });
+
+                      olho.mouseup(function() {
+                        senha.attr("type", "password");
+                      });
+                      // para evitar o problema de arrastar a imagem e a senha continuar exposta, 
+                      //citada pelo nosso amigo nos comentários
+                      $( "#olho" ).mouseout(function() { 
+                        $("#senha").attr("type", "password");
+                      });
+
+                    </script>
+
                   <div class="row">    <!-- Final Select Bairros -->
                       <div class="col-sm-12">
                           <div class="g-recaptcha" data-sitekey="6LfEhacaAAAAABLjT--1aIilzCidREblF9cwIm62"></div>
