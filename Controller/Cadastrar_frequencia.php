60c5ad241c1a705dc47be7a479f370e83aa3e2ef
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-08-23 10:40:40 -0300

    ajute hearder

--- Controller/Cadastrar_frequencia.php
+++ Controller/Cadastrar_frequencia.php
@@ -77,8 +77,8 @@ try {
             $_SESSION['status']=0;
             $_SESSION['mensagem']='Alguma coisa deu errado!';
             $_SESSION['erro_sql']=$e;
-            echo "$e";
-          //header("location: ../View/diario_frequencia.php?$url_get");
+          //   echo "$e";
+          header("location: ../View/diario_frequencia.php?$url_get");
 
          }
 
