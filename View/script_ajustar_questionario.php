5e91b798839fa9c076fad541a77d52a92ee93293
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-08-25 20:39:39 -0300

    ajuste

--- View/script_ajustar_questionario.php
+++ View/script_ajustar_questionario.php
@@ -2,18 +2,18 @@
 include'../Model/Conexao.php';
 
 	try {
-		$res=$conexao->query("SELECT * from questionario where origem_questionario_id='' ");
+		$res=$conexao->query("SELECT * from questionario where origem_questionario_id='muda' ");
 		foreach ($res as $key => $value) {
 			$id=$value['id'];
-			$res=$conexao->exec("UPDATE questionario set origem_questionario_id='$id' where id=$id   ");
+			$res=$conexao->exec("UPDATE questionario set origem_questionario_id='$id' where origem_questionario_id ='muda' AND id=$id   ");
 		echo"Questionario <br>";
 
 		}
 		
-		$res_q=$conexao->query("SELECT * from questao where origem_questionario_id='' ");
+		$res_q=$conexao->query("SELECT * from questao where origem_questionario_id='muda' ");
 		foreach ($res_q as $key => $value) {
 			$id=$value['questionario_id'];
-			$res=$conexao->exec("UPDATE questao set origem_questionario_id='$id' where questionario_id=$id   ");
+			$res=$conexao->exec("UPDATE questao set origem_questionario_id='$id' where origem_questionario_id ='muda' AND questionario_id=$id   ");
 			echo"Questão <br>";
 
 
