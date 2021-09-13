5ac68db08248a3a609a9b555775a169c95990959
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-09-13 14:29:05 -0300

    ordenado por nome da turma

--- Model/Aluno.php
+++ Model/Aluno.php
@@ -413,7 +413,7 @@ function verificar_frequencia($conexao,$idescola,$idturma,$iddisciplina,$profess
 		$res=$conexao->query("SELECT aluno.senha,aluno.email,aluno.nome as 'nome_aluno', aluno.idaluno, aluno.status as 'status_aluno', turma.nome_turma,turma.idturma
 		 FROM 
 			aluno, ano_letivo,turma where
-			 serie_id=$idserie and aluno_id=idaluno and turma_id=idturma and escola_id=$escola_id and status like'Ativo' ORDER by nome ASC limit $indice,50");
+			 serie_id=$idserie and aluno_id=idaluno and turma_id=idturma and escola_id=$escola_id and status like'Ativo' ORDER by turma.nome_turma, nome ASC limit $indice,50");
 		return $res;
 	}	
 
