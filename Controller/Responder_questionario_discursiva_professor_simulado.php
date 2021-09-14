9d0a555c0a731a5f2432f217d0ec607b9d2d52d5
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-09-14 18:12:48 -0300

    ajuste questionario

--- Controller/Responder_questionario_discursiva_professor_simulado.php
+++ Controller/Responder_questionario_discursiva_professor_simulado.php
@@ -16,7 +16,7 @@ $id = $_GET['id'];
 // $questao_id=$_GET['questao_id'];
  
 
-	$pesquisa_alt=$conexao->query("SELECT * FROM questao_simulado,alternativa_simulado WHERE 
+	$pesquisa_alt=$conexao->query("SELECT alternativa_simulado.correta, alternativa_simulado.id as 'idalternativa' , alternativa_simulado.nome  FROM questao_simulado,alternativa_simulado WHERE 
 		questao_simulado.nome like '%$questao_alternativa%' and 
 		questao_id=questao_simulado.id and 
 		alternativa_simulado.id =$id and
@@ -30,8 +30,9 @@ $id = $_GET['id'];
 			$resposta=0;
 		}
 
+		$idalternativa=$value['idalternativa'];
 		$texto_alternativa=$value['nome'];
-		$pesquisa=$conexao->exec("UPDATE alternativa_simulado set correta='$resposta' where nome='$texto_alternativa' and origem_questionario_id='$origem_questionario_id' ");
+		$pesquisa=$conexao->exec("UPDATE alternativa_simulado set correta='$resposta' where id=$idalternativa ");
 		$conta++;
 	}
 	if ($conta>0) {
