29cb59f34746406a4afe912b899128a691cf78b2
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-09-20 16:43:07 -0300

    ajuste

--- View/xplanejamento/receber.php
+++ View/xplanejamento/receber.php
@@ -1,12 +1,32 @@
 <?php 
+try {	
  $arquivo = file_get_contents('php://input');
   $json= json_decode($arquivo);
   // $phone= $json->phone;
   $mensagem_recebida=$json->Recipients->Email;
 
 $body = [
-        'status' => $mensagem_recebida
+        'status' => 'certo: '
     ];
-echo json_encode($json);
+
+
+$decodificado = json_decode($body);
+
+if (!$decodificado) {
+    die('JSON invalido');
+}
+ 
+$decodificado->status= $mensagem_recebida;
+
+echo json_encode($body);
+
+} catch (Exception $e) {
+	$body = [
+        'status' => 'error'
+    ];
+	echo json_encode($body);
+
+
+}
 
 ?>
