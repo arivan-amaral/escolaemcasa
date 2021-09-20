0e8ec93c65cd14c06ae3cb88201f599604e35a34
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-09-20 18:02:02 -0300

    ajuste

--- View/xplanejamento/receber.php
+++ View/xplanejamento/receber.php
@@ -1,28 +1,37 @@
 <?php 
+include "../../Model/Conexao.php";
+
 try {	
  $arquivo = file_get_contents('php://input');
-  $json= json_decode($arquivo);
-  // $phone= $json->phone;
-  $mensagem_recebida="teee: ".$json->FromEmail;
+ $json= json_decode($arquivo);
+
+        $descr_tipoprocesso =$json->descr_tipoprocesso;
+        $id_pro =$json->id_pro;
+        $tautor =$json->tautor;
+        $descr_situacaoprojeto =$json->descr_situacaoprojeto;
+        $descr_situacaoprojeto2 =$json->descr_situacaoprojeto2;
+        $obsgerais =$json->obsgerais;
+        $tusuario =$json->tusuario;
+        $pagina =$json->pagina;
+
+
+$conexao->exec("INSERT INTO xplanejamento(descr_tipoprocesso, id_pro, tautor, descr_situacaoprojeto, descr_situacaoprojeto2, obsgerais, tusuario, pagina)
+ VALUES ('$descr_tipoprocesso', '$id_pro', '$tautor', 
+ '$descr_situacaoprojeto', '$descr_situacaoprojeto2', '$obsgerais',
+ '$tusuario', '$pagina')")
+
 
 $body = [
-        'status' => 'certo'
+        'status' => 'sucesso'
     ];
-
-
-$decodificado = json_decode($body);
- 
-$decodificado->status= $mensagem_recebida;
-
-echo json_encode($decodificado);
-
+// $decodificado = json_decode($body);
+// $decodificado->status= $mensagem_recebida;
+echo json_encode($body);
 } catch (Exception $e) {
-	$body = [
-        'status' => 'error'
+     $body = [
+        'status' => 'erro'
     ];
-	echo json_encode($body);
-
-
+    echo json_encode($body);
 }
 
 ?>
