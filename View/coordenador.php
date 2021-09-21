51e568aa54c5bc6bac87c42d874eedf11f40bfcc
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-09-21 10:32:00 -0300

    ajuste relatorio para coordenadores

--- View/coordenador.php
+++ View/coordenador.php
@@ -19,6 +19,7 @@ if (!isset($_SESSION['idcoordenador'])) {
 
   include '../Model/Coordenador.php';
   include '../Model/Escola.php';
+  include '../Model/Aluno.php';
 
 ?>
 
@@ -290,11 +291,89 @@ if (!isset($_SESSION['idcoordenador'])) {
 
 
 
-   <!--  <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-10">
-       -->    
+   <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-10">
+ <div class="row">
+
         <?php 
+
+            $res_video=$conexao->query("SELECT * FROM visualizacao_video order by data_hora asc");
+            $minuto_aux=0;
+            foreach ($res_video as $key => $value) {
+              $minuto_aux=$minuto_aux+($value['minuto']/2); 
+             
+
+            }
+    ?>
+
+
+        <div class="col-lg-3 col-6">
+          <!-- small card -->
+          <div class="small-box bg-info">
+
+          <a href="mural.php" class="small-box-footer">
+            <div class="inner">
+              <h5><?php echo number_format(($minuto_aux), 2, '.', ''); ?> Minutos assistidos</h5>
+            </div>
+            <div class="icon">
+              <!-- <i class="fas fa-tag"></i> -->
+            </div>
+            Total de minutos assistidos de toda a rede <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>         
+
+
+
+             <?php 
+
+                $pes_aluno=$conexao->query("SELECT aluno_id ,(SELECT DISTINCT SUM(minuto) ) AS 'minutos' FROM visualizacao_video GROUP BY aluno_id ORDER by minutos DESC limit 1");
+              $nome_aluno="";
+              $minutos_aluno=0;
+                foreach ($pes_aluno as $key => $value) {
+                  $aluno_id=$value['aluno_id'];
+                  $minutos_aluno=$value['minutos']/2;
+
+                  $res_dados_aluno=$conexao->query("SELECT nome FROM aluno WHERE idaluno= $aluno_id limit 1");
+;
+                  foreach ($res_dados_aluno as $key2 => $value2) {
+                    $nome_aluno=$value2['nome'];
+                  }                
+
+                }
+        ?>
+
+        <div class="col-lg-3 col-6">
+          <!-- small card -->
+          <div class="small-box bg-info">
+
+          <a href="mural.php" class="small-box-footer">
+            <div class="inner">
+              <h5>1° lugar com <?php echo number_format(($minutos_aluno), 2, '.', ''); ?> Mininutos assistidos</h5>
+            </div>
+            <div class="icon">
+              <!-- <i class="fas fa-tag"></i> -->
+            </div>
+            <?php echo $nome_aluno; ?>
+            <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+
+
+      </div>
+
+  </div>
+</div>
+        <?php 
+
+
+
+
+
+
+
           //   $res_video=$conexao->query("SELECT * FROM visualizacao_video order by data_hora asc");
           //   $array_data_video=array();
           //   $minuto_aux=0;
