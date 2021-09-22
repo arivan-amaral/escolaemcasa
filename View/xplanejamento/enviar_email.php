7b2ccad39fc42bab60ac003113374aaa14a9ec22
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-09-22 12:48:17 -0300

    ajuste

--- View/xplanejamento/enviar_email.php
+++ View/xplanejamento/enviar_email.php
@@ -22,7 +22,8 @@ try {
     $mail->Username   = 'valleteclablem@gmail.com';                     //SMTP username
     $mail->Password   = 'MudancaValle!';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    // $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 
  
