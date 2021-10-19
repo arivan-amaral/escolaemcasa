13b1044fb9c4477541c378e3b80ed90222f8b78a
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-10-19 16:58:59 -0300

    ajuste script parecer

--- View/script_ajusta_parecer.php
+++ View/script_ajusta_parecer.php
@@ -6,13 +6,14 @@
 try {
 
 if (isset($_GET['tokem'])) {
-		// code...
 	$res=$conexao->query("SELECT * FROM nota_backup WHERE escola_id = 38 AND turma_id = 5675 AND periodo_id = 2 and avaliacao='av1' GROUP by aluno_id ");
 	
 	$conta_qnt_siglas=1;
 	foreach ($res as $key => $value) {
 		$aluno_id=$value['aluno_id'];
-		$conexao->exec("UPDATE nota set  WHERE escola_id = 38 AND turma_id = 5675 AND periodo_id = 2 and avaliacao='av3' and aluno_id=$aluno_id and parecer_descritivo !=''
+		$$parecer_descritivo=$value['parecer_descritivo'];
+
+		$conexao->exec("UPDATE nota set parecer_descritivo='$parecer_descritivo'  WHERE escola_id = 38 AND turma_id = 5675 AND periodo_id = 2 and avaliacao='av3' and aluno_id=$aluno_id and parecer_descritivo !=''
 		           ");
 
 
