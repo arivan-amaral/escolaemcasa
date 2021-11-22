abf78c1b3a5cc6b6e169ff45b70b5a77a5c585de
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-11-22 09:35:12 -0300

    ajuste boletim

--- View/boletim.php
+++ View/boletim.php
@@ -173,18 +173,18 @@ echo"<div class='pagebreak'> </div>";
       foreach ($res_alunos as $key => $value) {
         $idaluno=$value['idaluno'];
         $nome_aluno=$value['nome_aluno'];
-    // echo "$numero";
+    //echo "$numero";
           boletim_fund2($conexao,$idescola,$idturma,$idserie,$idaluno,$numero,$nome_aluno,$nome_escola,$nome_turma);
        
-       // if ($numero%2==0 ) {
-       //      echo "<br>";
-       //      echo "<br>";
-       //      echo "<br>";
-
+       if ($numero%3==0 ) {
+            echo "<div class='pagebreak'> </div>";
+            // echo "<br>";
+            // echo "<br>";
+            // echo"<br>";
+            // echo"<br>";
             
          
-       //    }
-
+          }
         // echo"<a href='boletim_individual.php?idescola=$idescola&idturma=$idturma&idserie=$idserie&idaluno=$idaluno&numero=$numero&nome_aluno=$nome_aluno&nome_escola=$nome_escola&nome_turma=$nome_turma'>IMPRIMIR - $nome_aluno</a> <br><br>";
         $numero++;
       }
