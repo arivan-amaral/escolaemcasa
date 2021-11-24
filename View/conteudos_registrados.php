551d1490bcad976bde26f9b3715800c12d2ad616
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-11-24 08:31:37 -0300

    ajustes diario de conteudos

--- View/conteudos_registrados.php
+++ View/conteudos_registrados.php
@@ -6,170 +6,6 @@
  function diario_conteudo($conexao,$idescola,$idturma,$iddisciplina,$idserie,$nome_disciplina,$nome_professor,$nome_turma,$nome_escola,$data_inicial,$data_final,$periodo){
 ?>
 
-<html xmlns:v="urn:schemas-microsoft-com:vml"
-xmlns:o="urn:schemas-microsoft-com:office:office"
-xmlns:w="urn:schemas-microsoft-com:office:word"
-xmlns:x="urn:schemas-microsoft-com:office:excel"
-xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
-xmlns="http://www.w3.org/TR/REC-html40">
-
-<head>
-<meta charset="UTF-8">
-<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
-<meta name=ProgId content=Word.Document>
-<meta name=Generator content="Microsoft Word 15">
-<meta name=Originator content="Microsoft Word 15">
-<link rel=File-List href="regitro_conteudo_arquivos/filelist.xml">
-<link rel=Edit-Time-Data href="regitro_conteudo_arquivos/editdata.mso">
-
-<style>
-  .Namerotate {
-    display:inline-block;
-    filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
-    -webkit-transform: rotate(270deg);
-    -ms-transform: rotate(270deg);
-    transform: rotate(270deg);
-    
-  }
-  
-  .tblborder, .tblborder td, .tblborder th{
-    border-collapse:collapse;
-    border:1px solid #000;
-  }
-  
-  .tblborder td, .tblborder th{
-    padding:20px 10px;
-  }
-  
-  .positionRi {
-    position: absolute;
-    top: 10px;
-    left: 5px;
-    /*right:0; */
-    width: 200px;
-    height: 150px;
-    /*border: 3px solid #73AD21;*/
-  }
-
-  .positionRi2 {
-    position: absolute;
-    top: 10px;
-    left: 20px; 
-    /*right:50; */
-    /* height: 150px;  
-    width: 40px;*/  
-    /*border: 3px solid #73AD21;*/
-}
-
-.positionRi3 {
-  position: absolute;
-  top: 1px;
-  left: 1px;
-  margin-top: 10px;
-  /*right:0; */
-  height: 20px;
-  width: 175px;
-  
-  /*border: 3px solid #73AD21;*/
-}
-
-.positionNumero {
-  position: relative;
-  /*top: 1px;
-  left: 1px;
-  margin-top: 10px;
-  right:0; */
-  height: 20px;
-  /* width: 50px; */
-  width: 5px;
-  
-  /*border: 3px solid #73AD21;*/
-}
-  
-  </style>
-
-<link rel=themeData href="regitro_conteudo_arquivos/themedata.thmx">
-<link rel=colorSchemeMapping
-href="regitro_conteudo_arquivos/colorschememapping.xml">
-
-<style>
-<!--
- /* Font Definitions */
- @font-face
-	{font-family:"Cambria Math";
-	panose-1:2 4 5 3 5 4 6 3 2 4;
-	mso-font-charset:0;
-	mso-generic-font-family:roman;
-	mso-font-pitch:variable;
-	mso-font-signature:3 0 0 0 1 0;}
-@font-face
-	{font-family:Calibri;
-	panose-1:2 15 5 2 2 2 4 3 2 4;
-	mso-font-charset:0;
-	mso-generic-font-family:swiss;
-	mso-font-pitch:variable;
-	mso-font-signature:-469750017 -1073732485 9 0 511 0;}
-@font-face
-	{font-family:"Tw Cen MT Condensed";
-	panose-1:2 11 6 6 2 1 4 2 2 3;
-	mso-font-charset:0;
-	mso-generic-font-family:swiss;
-	mso-font-pitch:variable;
-	mso-font-signature:7 0 0 0 3 0;}
- /* Style Definitions */
- p.MsoNormal, li.MsoNormal, div.MsoNormal
-	{mso-style-unhide:no;
-	mso-style-qformat:yes;
-	mso-style-parent:"";
-	margin-top:0cm;
-	margin-right:0cm;
-	margin-bottom:8.0pt;
-	margin-left:0cm;
-	line-height:107%;
-	mso-pagination:widow-orphan;
-	font-size:11.0pt;
-	font-family:"Calibri",sans-serif;
-	mso-ascii-font-family:Calibri;
-	mso-ascii-theme-font:minor-latin;
-	mso-fareast-font-family:Calibri;
-	mso-fareast-theme-font:minor-latin;
-	mso-hansi-font-family:Calibri;
-	mso-hansi-theme-font:minor-latin;
-	mso-bidi-font-family:"Times New Roman";
-	mso-bidi-theme-font:minor-bidi;
-	mso-fareast-language:EN-US;}
-.MsoChpDefault
-	{mso-style-type:export-only;
-	mso-default-props:yes;
-	font-family:"Calibri",sans-serif;
-	mso-ascii-font-family:Calibri;
-	mso-ascii-theme-font:minor-latin;
-	mso-fareast-font-family:Calibri;
-	mso-fareast-theme-font:minor-latin;
-	mso-hansi-font-family:Calibri;
-	mso-hansi-theme-font:minor-latin;
-	mso-bidi-font-family:"Times New Roman";
-	mso-bidi-theme-font:minor-bidi;
-	mso-fareast-language:EN-US;}
-.MsoPapDefault
-	{mso-style-type:export-only;
-	margin-bottom:8.0pt;
-	line-height:107%;}
-@page WordSection1
-	{size:595.3pt 841.9pt;
-	margin:70.85pt 3.0cm 70.85pt 3.0cm;
-	mso-header-margin:35.4pt;
-	mso-footer-margin:35.4pt;
-	mso-paper-source:0;}
-div.WordSection1
-	{page:WordSection1;}
--->
-</style>
-
-</head>
-
-<body lang=PT-BR style='tab-interval:35.4pt;word-wrap:break-word'>
-
 <div class=WordSection1>
 
 <table class=MsoNormalTable border=0 cellspacing=0 cellpadding=0 width=936
@@ -370,6 +206,8 @@ div.WordSection1
   color:black;mso-fareast-language:PT-BR'>&nbsp;<o:p></o:p></span></p>
   </td>
  </tr>
+
+
  <tr style='mso-yfti-irow:10;height:15.75pt'>
   <td width=66 nowrap style='width:49.65pt;border:solid windowtext 1.0pt;
   border-top:none;mso-border-left-alt:solid windowtext .5pt;mso-border-bottom-alt:
@@ -389,7 +227,7 @@ div.WordSection1
   mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
   color:black;mso-fareast-language:PT-BR'>Data<o:p></o:p></span></p>
   </td>
-  <td width=406 nowrap colspan=2 style='width:304.75pt;border-top:none;
+  <td width=406 nowrap colspan=5 style=' border-top:1.0pt;
   border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
   mso-border-top-alt:solid windowtext .5pt;mso-border-top-alt:solid windowtext .5pt;
   mso-border-bottom-alt:solid windowtext .5pt;mso-border-right-alt:solid windowtext .5pt;
@@ -401,16 +239,7 @@ div.WordSection1
 <?php echo "$nome_disciplina"; ?>
   <o:p></o:p></span></p>
   </td>
-  <td width=369 nowrap colspan=3 style='width:276.45pt;border-top:none;
-  border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-  mso-border-top-alt:solid windowtext .5pt;mso-border-top-alt:solid windowtext .5pt;
-  mso-border-bottom-alt:solid windowtext .5pt;mso-border-right-alt:solid windowtext .5pt;
-  padding:0cm 3.5pt 0cm 3.5pt;height:15.75pt'>
-  <p class=MsoNormal align=center style='margin-bottom:0cm;text-align:center;
-  line-height:normal'><span style='font-size:12.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
-  mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
-  color:black;mso-fareast-language:PT-BR'>ASSINATURA<o:p></o:p></span></p>
-  </td>
+ 
  </tr>
 
 <?php
@@ -443,7 +272,7 @@ foreach ($result_conteudo as $key => $value) {
       color:black;mso-fareast-language:PT-BR'>&nbsp;<o:p><?php echo"$data_conteudo"; ?></o:p></span></p>
       </td>  
 
-      <td width=406 nowrap colspan=2 style='width:304.75pt;border-top:none;
+      <td  colspan=5 style=' border-top:1.0pt;
       border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
       mso-border-top-alt:solid windowtext .5pt;mso-border-top-alt:solid windowtext .5pt;
       mso-border-bottom-alt:solid windowtext .5pt;mso-border-right-alt:solid windowtext .5pt;
@@ -454,16 +283,7 @@ foreach ($result_conteudo as $key => $value) {
       color:black;mso-fareast-language:PT-BR'>&nbsp;<o:p><?php echo"$descricao"; ?></o:p></span></p>
       </td>
 
-      <td width=369 nowrap colspan=3 style='width:276.45pt;border-top:none;
-      border-left:none;border-bottom:solid windowtext 1.0pt;border-right:solid windowtext 1.0pt;
-      mso-border-top-alt:solid windowtext .5pt;mso-border-top-alt:solid windowtext .5pt;
-      mso-border-bottom-alt:solid windowtext .5pt;mso-border-right-alt:solid windowtext .5pt;
-      padding:0cm 3.5pt 0cm 3.5pt;height:15.0pt'>
-      <p class=MsoNormal style='margin-bottom:0cm;line-height:normal'><span
-      style='font-size:10.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
-      mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
-      color:black;mso-fareast-language:PT-BR'>&nbsp;<o:p></o:p></span></p>
-      </td>
+     
  </tr>
 <?php 
 $conta++;
@@ -530,16 +350,14 @@ $conta++;
   <td width=143 style='border:none'></td>
   <td width=189 style='border:none'></td>
  </tr>
- <![endif]>
+ 
 </table>
 
 <p class=MsoNormal><o:p>&nbsp;</o:p></p>
 
 </div>
 
-</body>
 
-</html>
 <?php 
 }
 ?>
