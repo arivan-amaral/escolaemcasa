16c21fad5f984cadf84ea64b90ccbe052dcd9828
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-10 11:58:26 -0300

    ajuste layout parecer

--- View/parecer_descritivo.php
+++ View/parecer_descritivo.php
@@ -2,7 +2,111 @@
 include_once"../Model/Conexao.php";
 include_once"../Model/Escola.php";
 include_once"parecere_descritivo_cheche.php";
+?>
 
+<html xmlns:v="urn:schemas-microsoft-com:vml"
+xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns:w="urn:schemas-microsoft-com:office:word"
+xmlns:x="urn:schemas-microsoft-com:office:excel"
+xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
+xmlns="http://www.w3.org/TR/REC-html40">
+
+<head>
+<meta charset="UTF-8">
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+<meta name=ProgId content=Word.Document>
+<meta name=Generator content="Microsoft Word 15">
+<meta name=Originator content="Microsoft Word 15">
+<link rel=File-List href="regitro_conteudo_arquivos/filelist.xml">
+<link rel=Edit-Time-Data href="regitro_conteudo_arquivos/editdata.mso">
+
+<style>
+  .Namerotate {
+    display:inline-block;
+    filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
+    -webkit-transform: rotate(270deg);
+    -ms-transform: rotate(270deg);
+    transform: rotate(270deg);
+    
+  }
+  
+  .tblborder, .tblborder td, .tblborder th{
+    border-collapse:collapse;
+    border:1px solid #000;
+  }
+  
+  .tblborder td, .tblborder th{
+    padding:20px 10px;
+  }
+  
+  .positionRi {
+    position: absolute;
+    top: 10px;
+    left: 5px;
+    /*right:0; */
+    width: 200px;
+    height: 150px;
+    /*border: 3px solid #73AD21;*/
+  }
+
+  .positionRi2 {
+    position: absolute;
+    top: 10px;
+    left: 20px; 
+    /*right:50; */
+    /* height: 150px;  
+    width: 40px;*/  
+    /*border: 3px solid #73AD21;*/
+}
+
+.positionRi3 {
+  position: absolute;
+  top: 1px;
+  left: 1px;
+  margin-top: 10px;
+  /*right:0; */
+  height: 20px;
+  width: 175px;
+  
+  /*border: 3px solid #73AD21;*/
+}
+
+.positionNumero {
+  position: relative;
+  /*top: 1px;
+  left: 1px;
+  margin-top: 10px;
+  right:0; */
+  height: 20px;
+  /* width: 50px; */
+  width: 5px;
+  
+  /*border: 3px solid #73AD21;*/
+}
+  
+
+    table { page-break-inside:auto }
+    tr    { page-break-inside:avoid; page-break-after:auto }
+    thead { display:table-header-group }
+    tfoot { display:table-footer-group }
+
+
+@media print {
+
+    .pagebreak { page-break-before: always; } /* page-break-after works, as well */
+  }
+  </style>
+
+<link rel=themeData href="regitro_conteudo_arquivos/themedata.thmx">
+<link rel=colorSchemeMapping
+href="regitro_conteudo_arquivos/colorschememapping.xml">
+
+ 
+</head>
+
+<body lang=PT-BR style='tab-interval:35.4pt;word-wrap:break-word'>
+
+<?php
 $idescola=$_GET['idescola'];
 $idturma=$_GET['idturma'];
 $idserie=$_GET['idserie'];
@@ -14,14 +118,20 @@ foreach ($res as $key => $value) {
 $nome_disciplina='';
 
   $res_alunos=listar_aluno_da_turma_professor($conexao,$idturma,$idescola);
+  $conta=1;
   foreach ($res_alunos as $key => $value) {
     $idaluno=$value['idaluno'];
     $nome_aluno=$value['nome_aluno'];
     $nome_turma=$value['nome_turma'];
     parecere_descritivo_cheche($conexao,$idescola,$idturma,$idserie,$nome_disciplina,$nome_escola,$nome_aluno,$nome_turma,$idaluno); 
-  echo" <br>";
+    echo" <br>";
+    if ($conta%2==0) {
+      echo "<div class='pagebreak'> </div>";
+    }
+    $conta++;
   }
+?>
+</body>
+</html>
 
-
-
- ?>
+ 
--- View/parecere_descritivo_cheche.php
+++ View/parecere_descritivo_cheche.php
@@ -6,174 +6,10 @@
  function parecere_descritivo_cheche($conexao,$idescola,$idturma,$iddisciplina,$idserie,$nome_escola,$nome_aluno,$nome_turma,$idaluno){
 ?>
 
-<html xmlns:v="urn:schemas-microsoft-com:vml"
-xmlns:o="urn:schemas-microsoft-com:office:office"
-xmlns:w="urn:schemas-microsoft-com:office:word"
-xmlns:x="urn:schemas-microsoft-com:office:excel"
-xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
-xmlns="http://www.w3.org/TR/REC-html40">
-
-<head>
-<meta charset="UTF-8">
-<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
-<meta name=ProgId content=Word.Document>
-<meta name=Generator content="Microsoft Word 15">
-<meta name=Originator content="Microsoft Word 15">
-<link rel=File-List href="regitro_conteudo_arquivos/filelist.xml">
-<link rel=Edit-Time-Data href="regitro_conteudo_arquivos/editdata.mso">
-
-<style>
-  .Namerotate {
-    display:inline-block;
-    filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
-    -webkit-transform: rotate(270deg);
-    -ms-transform: rotate(270deg);
-    transform: rotate(270deg);
-    
-  }
-  
-  .tblborder, .tblborder td, .tblborder th{
-    border-collapse:collapse;
-    border:1px solid #000;
-  }
-  
-  .tblborder td, .tblborder th{
-    padding:20px 10px;
-  }
-  
-  .positionRi {
-    position: absolute;
-    top: 10px;
-    left: 5px;
-    /*right:0; */
-    width: 200px;
-    height: 150px;
-    /*border: 3px solid #73AD21;*/
-  }
-
-  .positionRi2 {
-    position: absolute;
-    top: 10px;
-    left: 20px; 
-    /*right:50; */
-    /* height: 150px;  
-    width: 40px;*/  
-    /*border: 3px solid #73AD21;*/
-}
-
-.positionRi3 {
-  position: absolute;
-  top: 1px;
-  left: 1px;
-  margin-top: 10px;
-  /*right:0; */
-  height: 20px;
-  width: 175px;
-  
-  /*border: 3px solid #73AD21;*/
-}
-
-.positionNumero {
-  position: relative;
-  /*top: 1px;
-  left: 1px;
-  margin-top: 10px;
-  right:0; */
-  height: 20px;
-  /* width: 50px; */
-  width: 5px;
-  
-  /*border: 3px solid #73AD21;*/
-}
-  
-  </style>
-
-<link rel=themeData href="regitro_conteudo_arquivos/themedata.thmx">
-<link rel=colorSchemeMapping
-href="regitro_conteudo_arquivos/colorschememapping.xml">
-
-<style>
-<!--
- /* Font Definitions */
- @font-face
-	{font-family:"Cambria Math";
-	panose-1:2 4 5 3 5 4 6 3 2 4;
-	mso-font-charset:0;
-	mso-generic-font-family:roman;
-	mso-font-pitch:variable;
-	mso-font-signature:3 0 0 0 1 0;}
-@font-face
-	{font-family:Calibri;
-	panose-1:2 15 5 2 2 2 4 3 2 4;
-	mso-font-charset:0;
-	mso-generic-font-family:swiss;
-	mso-font-pitch:variable;
-	mso-font-signature:-469750017 -1073732485 9 0 511 0;}
-@font-face
-	{font-family:"Tw Cen MT Condensed";
-	panose-1:2 11 6 6 2 1 4 2 2 3;
-	mso-font-charset:0;
-	mso-generic-font-family:swiss;
-	mso-font-pitch:variable;
-	mso-font-signature:7 0 0 0 3 0;}
- /* Style Definitions */
- p.MsoNormal, li.MsoNormal, div.MsoNormal
-	{mso-style-unhide:no;
-	mso-style-qformat:yes;
-	mso-style-parent:"";
-	margin-top:0cm;
-	margin-right:0cm;
-	margin-bottom:8.0pt;
-	margin-left:0cm;
-	line-height:107%;
-	mso-pagination:widow-orphan;
-	font-size:11.0pt;
-	font-family:"Calibri",sans-serif;
-	mso-ascii-font-family:Calibri;
-	mso-ascii-theme-font:minor-latin;
-	mso-fareast-font-family:Calibri;
-	mso-fareast-theme-font:minor-latin;
-	mso-hansi-font-family:Calibri;
-	mso-hansi-theme-font:minor-latin;
-	mso-bidi-font-family:"Times New Roman";
-	mso-bidi-theme-font:minor-bidi;
-	mso-fareast-language:EN-US;}
-.MsoChpDefault
-	{mso-style-type:export-only;
-	mso-default-props:yes;
-	font-family:"Calibri",sans-serif;
-	mso-ascii-font-family:Calibri;
-	mso-ascii-theme-font:minor-latin;
-	mso-fareast-font-family:Calibri;
-	mso-fareast-theme-font:minor-latin;
-	mso-hansi-font-family:Calibri;
-	mso-hansi-theme-font:minor-latin;
-	mso-bidi-font-family:"Times New Roman";
-	mso-bidi-theme-font:minor-bidi;
-	mso-fareast-language:EN-US;}
-.MsoPapDefault
-	{mso-style-type:export-only;
-	margin-bottom:8.0pt;
-	line-height:107%;}
-@page WordSection1
-	{size:595.3pt 841.9pt;
-	margin:70.85pt 3.0cm 70.85pt 3.0cm;
-	mso-header-margin:35.4pt;
-	mso-footer-margin:35.4pt;
-	mso-paper-source:0;}
-div.WordSection1
-	{page:WordSection1;}
--->
-</style>
-
-</head>
-
-<body lang=PT-BR style='tab-interval:35.4pt;word-wrap:break-word'>
-
 <div class=WordSection1>
 
-<table class=MsoNormalTable border=0 cellspacing=0 cellpadding=0 width=936
- style='width:701.7pt;border-collapse:collapse;mso-yfti-tbllook:1184;
+<table class=MsoNormalTable border=0 cellspacing=0 cellpadding=0  
+ style=' border-collapse:collapse;mso-yfti-tbllook:1184;
  mso-padding-alt:0cm 3.5pt 0cm 3.5pt'>
 
  <tr style='mso-yfti-irow:0;mso-yfti-firstrow:yes;height:20.25pt'>
@@ -547,13 +383,11 @@ echo "$parecer_descritivo";
 
 </table>
 
-<p class=MsoNormal><o:p>&nbsp;</o:p></p>
-
+ 
 </div>
 
-</body>
 
-</html>
+
 
 <?php 
   }
