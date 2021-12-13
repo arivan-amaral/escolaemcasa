868a5ee38e61ee15c8842590eae2e0b1cc65e2aa
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-13 09:04:15 -0300

    ajuste nf , apc

--- Controller/Lista_avaliacao_aluno_por_data.php
+++ Controller/Lista_avaliacao_aluno_por_data.php
@@ -2,7 +2,8 @@
   session_start();
     include("../Model/Conexao.php");
     include("../Model/Aluno.php");
-    include("../Controller/Conversao.php");
+    include("Nota_final_funcao.php");
+    include("Conversao.php");
     
 
 try {
@@ -181,22 +182,19 @@ try {
                                   }
 
                                 
-                                  $conta_aprovado=0;
-                                  if ($media <5) {
-                                    $res_conselho=buscar_aprovar_concelho($conexao,$idescola,$idturma,$iddisciplina,$idaluno);
-                                    $conta_aprovado=count($res_conselho);
-                                    
-                                    // if ($conta_aprovado>0 && $idperiodo==3) {
-                                    //   $media_conselho=5.0;
-                                    //   // code...
-                                    // }else{
-                                    //   $media_conselho=$media;
-                                    // }
-
                                     $result.="<label for='exampleInputEmail1' style='margin-left:10px;'>Total:</label>
                                     <input type='text'  value='$media' style='width:50px; background-color: #FFDAB9;'>"; 
-                                       
-                                       if ($idserie >3) {  
+                                  
+                                  $conta_aprovado=0;
+                                      $nf=0;
+                                      if ($idserie >3 && $idperiodo==3) {  
+                                          $nf=media_final($conexao,$idescola,$idturma,$iddisciplina,$idperiodo, $idaluno );
+                                      }
+                                  if ($nf <5) {
+                                    $res_conselho=buscar_aprovar_concelho($conexao,$idescola,$idturma,$iddisciplina,$idaluno);
+                                    $conta_aprovado=count($res_conselho);
+                                   
+                                       if ($idserie >3 && $idperiodo==3) {  
                                          $result.="
 
                                          <input type='hidden' value='$idescola' id='escola_apc$idaluno'>
@@ -211,12 +209,19 @@ try {
                                          
                                          if ($conta_aprovado>0) {
                                             //aprovar_concelho($conexao,$idescola,$idturma,$iddisciplina,$idaluno);
+                                            $result.="<label for='exampleInputEmail1' style='margin-left:10px;'>NF:</label>
+                                            <input type='text'  value='5.0' style='width:50px; background-color: #FFDAB9;'>"; 
+                                            
                                             $result.="
                                             <a class='btn btn-success'> APC </A>
                                             <a class='btn btn-danger' onclick='cancelar_aprovacao_concelho($idaluno);'> Cancelar aprovação</a>";
 
                                          }else{
                                           $result.="<a class='btn btn-primary' onclick='aprovar_concelho($idaluno);'> Aprovar pelo conselho</a>";
+
+                                          $result.="<label for='exampleInputEmail1' style='margin-left:10px;'>NF:</label>
+                                          <input type='text'  value='$nf' style='width:50px; background-color: #FFDAB9;'>"; 
+                                          
                                          }
 
                                            
@@ -227,13 +232,7 @@ try {
                                         <br>
                                     ";
 
-                                  }else{
-                                    $result.="<label for='exampleInputEmail1' style='margin-left:10px;'>Total:</label>
-                                    <input type='text'  value='$media' style='width:50px;background-color: #00BFFF;''>
-                                      <br>
-                                    ";
                                   }
-
 
 
                                   if (count($array_nota1)>1) {
---
+++ Controller/Nota_final_funcao.php
@@ -0,0 +1,121 @@
+<?php
+// include '../Model/Conexao.php';
+function media_final($conexao,$idescola,$idturma,$iddisciplina,$periodo_id, $idaluno ){
+        $result_nota_aula1=$conexao->query("
+          SELECT * FROM nota WHERE
+          escola_id=$idescola and
+          turma_id=$idturma and
+          disciplina_id=$iddisciplina and 
+          periodo_id=1 and aluno_id=$idaluno  group by avaliacao,periodo_id ");
+
+
+        $nota_tri_1=0;
+        $nota_av3_1='';
+        $nota_rp_1='';
+        foreach ($result_nota_aula1 as $key => $value) {
+
+          if ($value['avaliacao']!='RP') {
+            $nota_tri_1+=$value['nota'];
+
+
+          }
+            // ***************************************
+          if ($value['avaliacao']=='av3') {
+            $nota_av3_1=$value['nota'];
+
+          }
+
+          if ($value['avaliacao']=='RP') {
+            $nota_rp_1=$value['nota'];
+
+
+          }
+
+        }
+
+        if ($nota_tri_1<5 && $nota_rp_1!='' && $nota_rp_1>$nota_av3_1) {
+         $nota_tri_1=($nota_tri_1-$nota_av3_1)+$nota_rp_1;
+       }
+
+     //  echo "$nota_tri_1";
+     
+
+      $result_nota_aula2=$conexao->query("
+        SELECT * FROM nota WHERE
+        escola_id=$idescola and
+        turma_id=$idturma and
+        disciplina_id=$iddisciplina and 
+        periodo_id=2 and aluno_id=$idaluno  group by avaliacao,periodo_id ");
+
+
+      $nota_tri_2=0;
+      $nota_av3_2='';
+      $nota_rp_2='';
+      foreach ($result_nota_aula2 as $key => $value) {
+
+        if ($value['avaliacao']!='RP') {
+          $nota_tri_2+=$value['nota'];
+
+
+        }
+          // ***************************************
+        if ($value['avaliacao']=='av3') {
+          $nota_av3_2=$value['nota'];
+
+        }
+
+        if ($value['avaliacao']=='RP') {
+          $nota_rp_2=$value['nota'];
+
+
+        }
+
+      }
+
+      if ($nota_tri_2<5 && $nota_rp_2!='' && $nota_rp_2>$nota_av3_2) {
+       $nota_tri_2=($nota_tri_2-$nota_av3_2)+$nota_rp_2;
+     }
+
+//     echo "$nota_tri_2";
+
+
+   $result_nota_aula3=$conexao->query("
+     SELECT * FROM nota WHERE
+     escola_id=$idescola and
+     turma_id=$idturma and
+     disciplina_id=$iddisciplina and 
+     periodo_id=3 and aluno_id=$idaluno  group by avaliacao,periodo_id ");
+
+
+   $nota_tri_3=0;
+   $nota_av3_3='';
+   $nota_rp_3='';
+   foreach ($result_nota_aula3 as $key => $value) {
+
+     if ($value['avaliacao']!='RP') {
+       $nota_tri_3+=$value['nota'];
+
+
+     }
+       // ***************************************
+     if ($value['avaliacao']=='av3') {
+       $nota_av3_3=$value['nota'];
+
+     }
+
+     if ($value['avaliacao']=='RP') {
+       $nota_rp_3=$value['nota'];
+
+
+     }
+
+   }
+
+   if ($nota_tri_3<5 && $nota_rp_3!='' && $nota_rp_3>$nota_av3_3) {
+    $nota_tri_3=($nota_tri_3-$nota_av3_3)+$nota_rp_3;
+  }
+
+  $nf= ($nota_tri_1+$nota_tri_2+$nota_tri_3)/3;
+  //echo "$nota_tri_3";
+  return number_format($nf, 1, '.','');
+  }
