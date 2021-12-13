11636c7a855fee5eb319ea740c41faa7582c9a6c
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-13 15:02:08 -0300

    ajustes ata final

--- View/ata_resultado_final.php
+++ View/ata_resultado_final.php
@@ -4,6 +4,7 @@ include_once '../Controller/Conversao.php';
 include_once '../Model/Aluno.php';
 include_once '../Model/Turma.php';
 include_once 'ata_resultado_final_funcao.php';
+include_once '../Controller/Cauculos_notas.php';
 // include_once 'capa_turma_funcao.php';
  
 $idescola=$_POST['idescola'];
