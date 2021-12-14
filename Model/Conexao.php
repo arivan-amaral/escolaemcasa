e6ebe43887b383be0fae04a4d4556fb2cbf5ba0c
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-14 00:35:41 -0300

    ajuste conexao

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -1,18 +1,21 @@
 <?php
 date_default_timezone_set('America/Bahia');
 
-$servername = "localhost";
+$servername = "34.151.231.17";
 $username = "root";
-$password = "";
+$password = "Oaeh6h7H7m6EaB7F";
+
+
 // $password = "BDWRe85Oam8D";
 // $mysql  = "Oaeh6h7H7m6EaB7F";
 
 try {
-	//instancia objeto PDO, conectando no MySQL
+    //instancia objeto PDO, conectando no MySQL
     $conexao = new PDO("mysql:host=$servername;dbname=educalem", $username, $password);
     // apresenta o erro PDO 
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Conexao realizada com sucesso!"; 
+
 }catch(PDOException $e){
     echo "Conexao falhou: ";
 }
