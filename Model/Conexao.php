21627648a3c1b800c3328b303fc37a1d422848f9
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-14 05:02:50 -0300

    ajuste

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -1,19 +1,20 @@
 <?php
 date_default_timezone_set('America/Bahia');
 try {
-$data_hora="2021-12-14 06:00:00";// data de bloqueio
-$data_hora_atual=date("Y-m-d H:m:i"); //dataa tual
+// $data_hora="2021-12-14 06:00:00";// data de bloqueio
+// $data_hora_atual=date("Y-m-d H:m:i"); //dataa tual
 
-if ($data_hora <  $data_hora_atual) {//desbloquado para servidor padrao
+// if ($data_hora <  $data_hora_atual) {
+//desbloquado para servidor padrao
     $servername = "localhost";
     $username = "root";
     $password = "BDWRe85Oam8D";
-}else{
+// }else{
 
-$servername = "34.151.231.17";
-$username = "root";
-$password = "Oaeh6h7H7m6EaB7F";
-}
+// $servername = "34.151.231.17";
+// $username = "root";
+// $password = "Oaeh6h7H7m6EaB7F";
+// }
 
 
 // $password = "BDWRe85Oam8D";
