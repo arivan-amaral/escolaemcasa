169bf91685f07418ea79bcddca1800c4b880a741
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-14 01:01:46 -0300

    ajuste conexao

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -1,15 +1,24 @@
 <?php
 date_default_timezone_set('America/Bahia');
+try {
+$data_hora="2021-12-14 06:00:00";// data de bloqueio
+$data_hora_atual=date("Y-m-d H:m:i"); //dataa tual
+
+if ($data_hora <  $data_hora_atual) {//desbloquado para servidor padrao
+    $servername = "localhost";
+    $username = "root";
+    $password = "BDWRe85Oam8D";
+}else{
 
 $servername = "34.151.231.17";
 $username = "root";
 $password = "Oaeh6h7H7m6EaB7F";
+}
 
 
 // $password = "BDWRe85Oam8D";
 // $mysql  = "Oaeh6h7H7m6EaB7F";
 
-try {
     //instancia objeto PDO, conectando no MySQL
     $conexao = new PDO("mysql:host=$servername;dbname=educalem", $username, $password);
     // apresenta o erro PDO 
