4729782d3ec4b28a6d13bbfdff83923cb5894db4
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-17 08:12:26 -0300

    ajuste boletim

--- View/boletim.php
+++ View/boletim.php
@@ -183,7 +183,7 @@ echo"<div class='pagebreak'> </div>";
         //echo ". <input type='hidden' value='$numero'>";
         echo "<input type='hidden' name='$numero' value='$numero'><br>";
 
-    // echo "$numero";
+     echo "$numero";
           boletim_fund2($conexao,$idescola,$idturma,$idserie,$idaluno,$numero,$nome_aluno,$nome_escola,$nome_turma);
        
        if ($numero%3==0 ) {
