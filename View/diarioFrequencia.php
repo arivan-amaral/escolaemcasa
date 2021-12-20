0db6cf18f39f47df4236c46ab5bda517c00a63d3
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-20 15:40:29 -0300

    ajuste

--- View/diarioFrequencia.php
+++ View/diarioFrequencia.php
@@ -373,7 +373,7 @@ foreach ($result_data_aula as $key => $value) {
       border-left:none;background:#D9D9D9;mso-border-left-alt:solid windowtext 1.0pt;mso-border-alt:
       solid windowtext 1.0pt;mso-border-right-alt:solid windowtext .5pt;padding:0cm 0pt 0cm 0pt;mso-rotate:90; '>
       <p class=MsoNormal align=center style='margin-bottom:0cm;text-align:center;
-      line-height:normal'><div class="Namerotate"><span style='font-size:10.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
+      line-height:normal'><div class="Namerotate"><span style='font-size:6.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
       mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Arial;
       color:black;mso-fareast-language:PT-BR'><?php echo "".converte_data($data_frequencia); ?> </div></span></p>
   </td>
