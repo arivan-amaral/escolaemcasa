3f433c3b1a5943c7ed0177bc1e7480c68f3c0b90
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2021-12-22 09:58:29 -0300

    ajuste

--- Controller/Cadastrar_diario_avaliacao_aluno.php
+++ Controller/Cadastrar_diario_avaliacao_aluno.php
@@ -33,75 +33,7 @@ try {
     $nota=0;
     $url_get=$_POST['url_get'];
 
-    /////////////////////////////////////////////////////////
-      
-        if(
-         
-         $_SESSION['idprofessor']==409 ||
-         $_SESSION['idprofessor']==523 ||
-         $_SESSION['idprofessor']==772 ||
-         $_SESSION['idprofessor']==1401 ||
-         $_SESSION['idprofessor']==733 ||
-         $_SESSION['idprofessor']==767 ||
-         $_SESSION['idprofessor']==575 ||
-         $_SESSION['idprofessor']==768 ||
-         $_SESSION['idprofessor']==523 ||
-         $_SESSION['idprofessor']==769 ||
-         $_SESSION['idprofessor']==1394 ||
-         $_SESSION['idprofessor']==337 ||
-         $_SESSION['idprofessor']==586 ||
-         $_SESSION['idprofessor']==578 ||
-         $_SESSION['idprofessor']==516 ||
-
-         $_SESSION['idprofessor']==1074 ||
-         $_SESSION['idprofessor']==1001 ||
-         $_SESSION['idprofessor']==585 ||
-         $_SESSION['idprofessor']==584 ||
-         $_SESSION['idprofessor']==576 ||
-         $_SESSION['idprofessor']==570 ||
-         $_SESSION['idprofessor']==582 ||
-         $_SESSION['idprofessor']==505 ||
-         $_SESSION['idprofessor']==517 ||
-         $_SESSION['idprofessor']==376 ||
-         
-
-
-         $_SESSION['idprofessor']==645 ||
-         $_SESSION['idprofessor']==281 ||
-         $_SESSION['idprofessor']==325 ||
-
-         $_SESSION['idprofessor']==514 ||
-         $_SESSION['idprofessor']==485 || 
-         $_SESSION['idprofessor']==467 || 
-         $_SESSION['idprofessor']==718 || 
-         $_SESSION['idprofessor']==1416 || 
-         $_SESSION['idprofessor']==895 ||
-         $_SESSION['idprofessor']==972 ||
-         $_SESSION['idprofessor']==679 ||
-         $_SESSION['idprofessor']==686 || 
-         $_SESSION['idprofessor']==305 ||
-         $_SESSION['idprofessor']==722 ||
-         $_SESSION['idprofessor']==907 ||
-         $_SESSION['idprofessor']==867 ||
-         $_SESSION['idprofessor']==501
-     ) {
-///
-        }elseif (isset($_SESSION['cargo'])) {
-        
-        if (isset($_SESSION['idprofessor'])) {
-                $_SESSION['status']=0;
-            $_SESSION['mensagem']='BLOQUEADO PARA PROFESSOR!';
-            header("location: ../View/diario_avaliacao.php?$url_get");
-            exit;
-        }
-    }else{
-                $_SESSION['status']=0;
-        
-            $_SESSION['mensagem']='BLOQUEADO PARA PROFESSOR!';
-          header("location: ../View/diario_avaliacao.php?$url_get");
-            exit;
-    }
-    /////////////////////////////////////////////////////////
+ include_once"Bloqueio_funcoes_para_professor.php";
 
 foreach ($_POST['aluno_id'] as $key => $value) {
       $aluno_id=$_POST['aluno_id'][$key];
