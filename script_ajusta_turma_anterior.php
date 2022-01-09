e775b6ae05f9d5f364447f48bfed2c8255522a8e
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-01-09 18:25:35 -0300

    AJUSTES SCRIPT

--- script_ajusta_turma_anterior.php
+++ script_ajusta_turma_anterior.php
@@ -2,12 +2,12 @@
 set_time_limit(0);
 include 'Model/Conexao.php';
 
-$res=$conexao->query("SELECT * from ecidade_matricula where matricula_turma !='' and turma_id !='' group by matricula_turma");
+$res=$conexao->query("SELECT * from ecidade_matricula where matricula_turma IS NOT NULL and turma_id IS NOT NULL group by matricula_turma");
 
 foreach ($res as $key => $value) {
 	$matricula_turma=$value['matricula_turma'];
 	$turma_id=$value['turma_id'];
 	$conexao->exec("UPDATE ecidade_matricula set turma_id_anterior= $turma_id WHERE matricula_turmaant=$matricula_turma");
-	
+
 
 }
