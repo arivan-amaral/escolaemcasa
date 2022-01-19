1e970771295472bf9f7f259bbbe192624514bf60
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-01-19 09:17:50 -0300

    ajustes rematricula

--- Controller/Rematricular_aluno.php
+++ Controller/Rematricular_aluno.php
@@ -53,14 +53,25 @@ try {
 			 	$matricula_tipo='R';
 			 	$calendario_ano=$_SESSION['ano_letivo_vigente'];
 
-			 if ( $quantidade_vagas_restante> 0 && $rematricula_serie_id ==$rematricula_nova_serie && ($resultado !="Apc" || $resultado !="Apr" ) ) {
+			 	$verificar_aluno_na_turna_rematricula=$conexao->query("SELECT * FROM ecidade_matricula where 
+			 		calendario_ano='$calendario_ano' and aluno_id=$aluno_id ");
+			
+			$rematriculado=0;
+			foreach ($verificar_aluno_na_turna_rematricula as $key => $value) {
+			 	$rematriculado++;
+			}
+
+
+			
+
+			 if ($rematriculado==0 && $quantidade_vagas_restante> 0 && $rematricula_serie_id ==$rematricula_nova_serie && ($resultado !="Apc" || $resultado !="Apr" ) ) {
 				rematricular_aluno($conexao,$aluno_id,$turma_id,$turma_id_anterior,$matricula_situacao,$matricula_concluida,$matricula_datamatricula,$matricula_ativa,$matricula_tipo,$calendario_ano,$turma_escola,$turno_nome);
 				
 				mudar_situacao_rematricular_aluno($conexao,$matricula_aluno);
 				$quantidade_vagas_restante--;
 
 
-			}elseif ($quantidade_vagas_restante> 0 && ($resultado=="Apc" || $resultado=="Apr") ) {
+			}elseif ($rematriculado==0 && $quantidade_vagas_restante> 0 && ($resultado=="Apc" || $resultado=="Apr") ) {
 			  	
 			  	rematricular_aluno($conexao,$aluno_id,$turma_id,$turma_id_anterior,$matricula_situacao,$matricula_concluida,$matricula_datamatricula,$matricula_ativa,$matricula_tipo,$calendario_ano,$turma_escola,$turno_nome);
 				
