1759736e52777dfe57b1617d9e44debf0fb04ce2
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-01-24 10:50:50 -0300

    ajustes

---
+++ Controller/Excluir_duplicidade_migracao.php
@@ -0,0 +1,29 @@
+<?php 
+set_time_limit(0);
+// session_start();
+include'../Model/Conexao.php';
+include'../Model/Aluno.php';
+include'../Model/Turma.php';
+include'Conversao.php';
+$indice=$_GET['indice'];
+$limite=$_GET['limite'];
+
+$res_aluno=$conexao->query("SELECT * FROM aluno limit $indice,$limite");
+foreach ($res_aluno as $key => $value) {
+    $idaluno=$value['idaluno'];
+    
+    $res=$conexao->query("SELECT * FROM ecidademigrado_alunos WHERE aluno_id=$idaluno limit 1");
+     $res=$res->fetchAll();
+     if(count($res)==0){
+        $res=$conexao->exec("DELETE FROM aluno WHERE idaluno=$idaluno");
+     }
+
+}
+
+
+} catch (Exception $e) {
+    echo $e;
+ 	 //$_SESSION['status']=0;
+	// header("location:../View/cadastro_aluno.php");
+}
+?>
