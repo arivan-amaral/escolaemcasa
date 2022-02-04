b3649e55be667aa27678710f816eff1e70592264
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-02-04 00:36:58 -0300

    ajustes

--- View/impressao_diario_frequencia.php
+++ View/impressao_diario_frequencia.php
@@ -1,8 +1,24 @@
 <?php 
 session_start();
 try {
-    
-echo "certo";
+include_once"../Controller/Conversao.php";
+include_once"../Model/Conexao.php";
+include_once"../Model/Coordenador.php";
+include_once"../Model/Aluno.php";
+include_once"../Model/EScola.php";
+
+include_once"diarioFrequencia_infantil.php";
+include_once"diarioFrequencia_fund1.php";
+include_once"diarioFrequencia_fund2.php";
+
+include_once"diarioFrequenciaPaginaFinal_infantil.php";
+include_once"diarioFrequenciaPaginaFinal_fund1.php";
+include_once"diarioFrequenciaPaginaFinal_fund2.php";
+ 
+$ano_letivo=$_SESSION['ano_letivo'];
+
+
+
 
 } catch (Exception $e) {
     echo "erro: $e";
