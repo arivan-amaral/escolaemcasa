b320013a805336400111e3d1f57bb30b46367a2d
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-03-10 12:00:47 -0300

    excluir conteu, frequencia apenas o proprietario

--- Controller/Excluir_frequencia.php
+++ Controller/Excluir_frequencia.php
@@ -3,6 +3,7 @@ session_start();
 include '../Model/Conexao.php';
 include '../Model/Aluno.php';
 
+$idprofessor=$_SESSION['idfuncionario'];
 $conteudo_aula_id=$_GET['conteudo_aula_id'];
 $local=$_GET['local'];
 
@@ -12,7 +13,7 @@ $array_url=explode('disc=', $_SERVER["REQUEST_URI"]);
 
 try {
 
-	excluir_frequencia_lancada($conexao,$conteudo_aula_id);
+	excluir_frequencia_lancada($conexao,$conteudo_aula_id,$idprofessor);
 	// $_SESSION['status']=1;
 	// header("Location:../View/$local.php?$url_get");
 } catch (Exception $e) {
--- Model/Aluno.php
+++ Model/Aluno.php
@@ -396,11 +396,11 @@ function verificar_conteudo_aula_cadastrado_por_data_aula($conexao, $iddisciplin
 
 
 
-function excluir_frequencia_lancada($conexao,$conteudo_aula_id) {
-    $conexao->exec("DELETE FROM frequencia WHERE conteudo_aula_id=$conteudo_aula_id
+function excluir_frequencia_lancada($conexao,$conteudo_aula_id,$idprofessor) {
+    $conexao->exec("DELETE FROM frequencia WHERE conteudo_aula_id=$conteudo_aula_id and professor_id=$idprofessor
       ");
     $conexao->exec("DELETE FROM conteudo_aula WHERE
-       id=$conteudo_aula_id
+       id=$conteudo_aula_id and professor_id=$idprofessor
        ");
 
 
