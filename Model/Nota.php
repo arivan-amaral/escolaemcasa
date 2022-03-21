7c15e466354ca5c7e6d269d15cceae1a982af12f
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-03-21 09:06:52 -0300

    teste: GROUP BY pesquisa_nota_por_periodo

--- Model/Nota.php
+++ Model/Nota.php
@@ -1,11 +1,13 @@
 <?php 
 	function pesquisa_nota_por_periodo($conexao,$idescola,$idturma,$iddisciplina,$idaluno,$periodo_id){
 		$result=$conexao->query("
-        SELECT * FROM nota WHERE
+        SELECT
+        avaliacao,periodo_id,escola_id,nota
+         FROM nota WHERE
         escola_id=$idescola and
         turma_id=$idturma and
         disciplina_id=$iddisciplina and 
-        periodo_id=$periodo_id and aluno_id=$idaluno  group by avaliacao,periodo_id ");
+        periodo_id=$periodo_id and aluno_id=$idaluno  group by avaliacao,periodo_id,nota ");
 
 		return $result;
 	}
