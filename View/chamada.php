73d19877d5342dfbdf90bf96461a9f10f6e39eba
danielRsantana99 <81334514+danielRsantana99@users.noreply.github.com>
2022-05-26 07:53:42 -0300

    Update chamada.php

--- View/chamada.php
+++ View/chamada.php
@@ -28,7 +28,26 @@ if (!isset($_SESSION['idcoordenador'])) {
 
   include '../Model/Setor.php';
   include '../Model/Chamada.php';
-
+   $_SESSION['total_chamados'] = 0;
+  $idEscola = 0 ;
+  $res_verificar_funcionario = buscar_setor_funcionario($conexao,$_SESSION['idfuncionario']);
+  foreach ($res_verificar_funcionario as $key => $value) {
+    $setor_id = $value['setor_id'];
+    $res_quantidade_pendente = quantidade_chamada_pendente($conexao,$setor_id);
+    foreach ($res_quantidade_pendente as $key => $value) {
+      $_SESSION['total_chamados'] += $value['chamada'];
+    }
+  }
+  $res_verificar_escola = buscar_id_escola($conexao,$_SESSION['idfuncionario']);
+  foreach ($res_verificar_escola as $key => $value) {
+    $idEscola = $value['escola_id'];
+  }
+  if ($idEscola != 0) {
+    $res_escola =  quantidade_chamada_pendente_escola($conexao,11,$idEscola);
+    foreach ($res_escola as $key => $value) {
+      $_SESSION['total_chamados'] += $value['chamada'];
+    }
+  }
 if ($_COOKIE['dia_doservidor_publico2']<2 && date("m-d")=="10-28") {
 ?>
     <script>
