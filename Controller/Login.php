14da99d3a5c3bee19a520189ba0fb9d24650ee82
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-10-15 14:57:42 -0300

    ajustes

--- Controller/Login.php
+++ Controller/Login.php
@@ -231,7 +231,7 @@ try {
   $_SESSION['status']=0;
   $_SESSION['mensagem']="Algo deu errado, confira seus dados de acesso e tente novamente!";
   //echo "ESTAMOS EM MANUTENÇÃO:";
-  header("Location:../View/");
+ // header("Location:../View/");
 }
 
 
