e77d14a8f79c1ebef208c35841f01b4d75f63d4b
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-10-26 11:34:20 -0300

    ajustes

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -7,7 +7,7 @@ date_default_timezone_set('America/Sao_Paulo');
      
      $password = "Ari200120022003_";
    
-    $conexao = new PDO("mysql:host=$server;dbname=educalem;charset=utf8mb4", $username, $password);
+    $conexao = new PDO("mysql:host=$server;dbname=educalem;charset=utf8", $username, $password);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Falha na conexão:";
