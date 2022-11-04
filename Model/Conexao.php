4d2042c581029ea0f075242403d176096ad32942
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-11-04 07:27:01 -0300

    novo usuario bd

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -3,10 +3,10 @@ try {
 date_default_timezone_set('America/Sao_Paulo');
     
     $server = "localhost";
-    $username = "root";
+    $username = "educ_alem";
      
      $password = "DLsaJ#pBpK4K@K4x";
-   
+    
     $conexao = new PDO("mysql:host=$server;dbname=educ_alem;charset=utf8", $username, $password);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
