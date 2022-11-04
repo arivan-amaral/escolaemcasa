c3ef4d2084bc117059ead2879aca6ab528b49380
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-11-04 07:18:36 -0300

    nova senha bd

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -2,12 +2,12 @@
 try {
 date_default_timezone_set('America/Sao_Paulo');
     
-    $server = "177.153.50.141";
+    $server = "localhost";
     $username = "root";
      
-     $password = "Ari200120022003_";
+     $password = "DLsaJ#pBpK4K@K4x";
    
-    $conexao = new PDO("mysql:host=$server;dbname=educalem;charset=utf8", $username, $password);
+    $conexao = new PDO("mysql:host=$server;dbname=educ_alem;charset=utf8", $username, $password);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Falha na conexão:";
