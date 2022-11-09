9bf826a0c9a914c5fe60e6de168d25c3829ef77c
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-11-09 09:42:49 -0300

    ajustes

--- Model/Escola.php
+++ Model/Escola.php
@@ -30,7 +30,7 @@ function pesquisa_matricula_mensal($conexao,$escola,$ano){
 
 function pesquisa_relatorio_filtro($conexao,$texto,$escola,$ano_letivo){
    $sql = $conexao->query("SELECT $texto FROM aluno,ecidade_matricula,escola,turma WHERE ecidade_matricula.aluno_id = aluno.idaluno AND ecidade_matricula.turma_escola = escola.idescola AND ecidade_matricula.turma_id = turma.idturma AND ecidade_matricula.calendario_ano='$ano_letivo' AND ecidade_matricula.matricula_ativa='S' AND ecidade_matricula.matricula_concluida='N'   AND ecidade_matricula.turma_escola=$escola ORDER BY escola.nome_escola ASC, turma.nome_turma ASC, aluno.nome asc;");
-   echo "SELECT $texto FROM aluno,ecidade_matricula,escola,turma WHERE ecidade_matricula.aluno_id = aluno.idaluno AND ecidade_matricula.turma_escola = escola.idescola AND ecidade_matricula.turma_id = turma.idturma AND ecidade_matricula.calendario_ano='$ano_letivo' AND ecidade_matricula.matricula_ativa='S' AND ecidade_matricula.matricula_concluida='N'   AND ecidade_matricula.turma_escola=$escola ORDER BY escola.nome_escola ASC, turma.nome_turma ASC, aluno.nome asc;";
+   
    return $sql->fetchAll();
 }
 
