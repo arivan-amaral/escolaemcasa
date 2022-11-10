663a6afaa6ae50b58279c4202033d0f4b27d5f42
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2022-11-10 14:35:23 -0300

    ajustes

--- Model/Escola.php
+++ Model/Escola.php
@@ -39,7 +39,7 @@ function pesquisa_relatorio_filtro_todos($conexao,$texto,$escola,$ano_letivo){
    return $sql->fetchAll();
 }
 function pesquisa_relatorio_filtro_quantidade_sexo($conexao,$escola,$ano_letivo,$idturm){
-   $sql = $conexao->query("SELECT aluno.sexo, count(*) as 'quantidade' FROM aluno,ecidade_matricula,escola,turma WHERE ecidade_matricula.aluno_id = aluno.idaluno AND ecidade_matricula.turma_escola = escola.idescola AND ecidade_matricula.turma_id = turma.idturma AND ecidade_matricula.calendario_ano='$ano_letivo' AND ecidade_matricula.matricula_ativa='S'AND ecidade_matricula.matricula_ativa='S' and ecidade_matricula.turma_id='$idturm'  AND ecidade_matricula.matricula_concluida='N'   AND ecidade_matricula.turma_escola=$escola GROUP BY aluno.sexo ORDER BY  turma.nome_turma ASC;");
+   $sql = $conexao->query("SELECT aluno.sexo, count(*) as 'quantidade' FROM aluno,ecidade_matricula,escola,turma WHERE ecidade_matricula.aluno_id = aluno.idaluno AND ecidade_matricula.turma_escola = escola.idescola AND ecidade_matricula.turma_id = turma.idturma AND ecidade_matricula.calendario_ano='$ano_letivo' AND ecidade_matricula.matricula_ativa='S'AND ecidade_matricula.matricula_ativa='S' and ecidade_matricula.turma_id='$idturm'  AND ecidade_matricula.matricula_concluida='N'   AND ecidade_matricula.turma_escola=$escola GROUP BY aluno.sexo ORDER BY  aluno.sexo ASC;");
    
    return $sql->fetchAll();
 }
