32a905722c3785290a2aad195790732bcf7d7200
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-01-04 11:15:12 -0300

    AJUSTES

--- Model/Aluno.php
+++ Model/Aluno.php
@@ -1586,6 +1586,7 @@ ecidade_matricula.calendario_ano ='$ano_letivo' and
 ecidade_matricula.matricula_concluida='S' and
 ecidade_matricula.matricula_ativa='N' and
 ecidade_matricula.turma_escola=$escola_id and
+ecidade_matricula.matricula_situacao !='CANCELADO' and
 ecidade_matricula.turma_id=$turma_id  ORDER by aluno.nome ASC");
 
 
