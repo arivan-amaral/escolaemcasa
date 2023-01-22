6ddc852c1272c00ff00d9a52972f9ba6992bec1b
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-01-22 19:11:56 -0300

    pausar banco

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -3,9 +3,9 @@ try {
 date_default_timezone_set('America/Sao_Paulo');
     
     $server = "localhost";
-    $username = "educ_lem";
+    $username = "root";
      
-     $password = "Ari200120022003_";
+     $password = "";
     
     $conexao = new PDO("mysql:host=$server;dbname=educ_lem;charset=utf8", $username, $password);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
