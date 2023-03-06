576755f82269f37a0e316c3d1a64c860e4683bae
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-03-06 10:47:09 -0300

    ajustes

--- Controller/Lista_carteirinha_escola.php
+++ Controller/Lista_carteirinha_escola.php
@@ -47,16 +47,57 @@ if ($caminho_foto_carteirinha !="") {
 
         <br>
         <div class='row'>
-            <div class='col-sm-5 div_carteirinha '>
+            <div class='col-sm-4 div_carteirinha '>
                   <div class='row'>
 
                 
                         
-                        <div class='col-sm-2 imagem_perfil' >
+                        <div class='col-sm-3 imagem_perfil' >
                           <img src='$caminho_foto_carteirinha' >
                         </div>
                         
-                        <div class='col-sm-10 dados_aluno'>
+                        <div class='col-sm-9 dados_aluno'>
+                            <b class='nome_aluno'>$nome_aluno</b><br>
+                          <p class='outros_dados_aluno'> 
+                          <b>$nome_escola</b> <br>
+                          <b>TURMA: $nome_turma</b><br>
+                          <b>RESPONSÁVEL: $nome_responsavel </b><br>
+                          <b>DATA NASC: $data_nascimento</b><br>
+                          <b>MATRÍCULA: $matricula_aluno</b><br>
+                          <b>CPF/RG:</b><br>
+                          <b>TIPO SANGUINEO: </b><br>
+                          <b>TELEFONE: $whatsapp_responsavel</b><br>
+                             <b>LINHA:$linha_transporte</b>
+                        </p>
+                         
+
+                        </div>
+
+                  </div> 
+
+                   <div class='row'>
+
+                        <div class='col-sm-10'></div>
+                        <div class='col-sm-2 validade'> <b>".$_SESSION['ano_letivo_vigente']."</b> </div>
+                        
+                        
+             
+
+                  </div>
+                       
+
+            </div>                
+
+            <div class='col-sm-4 div_carteirinha '>
+                  <div class='row'>
+
+                
+                        
+                        <div class='col-sm-3 imagem_perfil' >
+                          <img src='$caminho_foto_carteirinha' >
+                        </div>
+                        
+                        <div class='col-sm-9 dados_aluno'>
                             <b class='nome_aluno'>$nome_aluno</b><br>
                           <p class='outros_dados_aluno'> 
                           <b>$nome_escola</b> <br>
@@ -88,31 +129,8 @@ if ($caminho_foto_carteirinha !="") {
 
             </div>             
             
-            <div class='col-sm-1 '></div>
 
-            <div class='col-sm-5 div_carteirinha '>
-                  <div class='row'>
 
-                        <div class='col-sm-1'></div>
-                        
-                        <div class='col-sm-3 imagem_perfil' >
-                          <img src='imagens/avatar_carteirinha.png'  >
-                        </div>
-                        
-                        <div class='col-sm-8 dados_aluno'>
-                          <b >RESPONSÁVEL:</b><br>
-                          <b>DATA NASC:</b><br>
-                          <b>MATRÍCULA:</b><br>
-                          <b>CPF/RG:</b><br>
-                          <b>TIPO SANGUINEO:</b><br>
-                          <b>TELEFONE:</b><br>
-
-                        </div>
-
-                  </div>
-                       
-
-            </div> 
 
 
             
