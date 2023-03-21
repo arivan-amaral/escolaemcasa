20098ed4c875ae08f6597baf570693da49943932
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-03-21 17:46:19 -0300

    teste: historico

--- View/historico_aluno.php
+++ View/historico_aluno.php
@@ -5,6 +5,11 @@ include_once "../Model/Disciplina.php";
 include_once '../Model/Escola.php';
 include_once '../Model/Aluno.php';
 include_once '../Controller/Cauculos_notas.php';
+
+
+  include_once'historico.php';
+ 
+  include_once'../Model/Nota.php';
 ?>
  <!-- <H1> <font color='red'>PÁGINA EM MANUTENÇÃO</font> </H1><BR> -->
 
@@ -56,28 +61,28 @@ text-shadow:none;
 
 </p>
 <?php
-  include_once'historico.php';
-  include_once'../Model/Conexao.php';
-  include_once'../Model/Nota.php';
+
   $idaluno=$_GET['idaluno'];
   $idserie=$_GET['idserie'];
   $idescola=$_GET['idescola'];
 
 
-  // $resultado=registrar_sistema_atual_nota_historico_($conexao, $idaluno, $ano);
 
-   
-  // foreach ($resultado as $key => $value) {
-  //   $idescola=$value['idescola'];
-  //   $idturma=$value['idturma'];
-  //   $idserie=$value['idserie'];
-  //   $matricula=$value['matricula'];
-  //   $calendario_ano=$value['calendario_ano'];
-  //   $matricula_situacao=$value['matricula_situacao'];
-    // $media=gerar_media_ata($conexao,$idescola,$idturma,$iddisciplina,$idaluno,$ano_letivo,$idserie);
-  // }
+if ($_SESSION['nivel_acesso_id']==1010) {
+  for ($ano_conta=2021; $ano_conta <= date("Y") ; $ano_conta+) { 
+    $resultado=registrar_sistema_atual_nota_historico_($conexao, $idaluno, $ano_conta); 
+    foreach ($resultado as $key => $value) {
+      $idescola=$value['idescola'];
+      $idturma=$value['idturma'];
+      $idserie=$value['idserie'];
+      $matricula=$value['matricula'];
+      $calendario_ano=$value['calendario_ano'];
+      $matricula_situacao=$value['matricula_situacao'];
+      $media=gerar_media_ata($conexao,$idescola,$idturma,$iddisciplina,$idaluno,$ano_letivo,$idserie);
+    }
 
-
+  }
+}
 
 
   hitorico_aluno($conexao,$idaluno,$idserie,$idescola);
