f10bd34ae32e6fdadff1eb8d2328a7b360003c04
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-04-03 09:43:22 -0300

    ajustes

--- Controller/Editar_aluno.php
+++ Controller/Editar_aluno.php
@@ -291,6 +291,39 @@ try {
             $destino = '../View/imagem_carteirinha_transporte/' . $novoNome;
             if(move_uploaded_file($arquivo_tmp, $destino)){
                 alterar_foto_carterinha_aluno($conexao, $novoNome, $idaluno);
+
+ 
+
+                $new_width = 800;
+                $new_height = 700;
+
+                // Carregue a imagem original
+                $original_image = imagecreatefromjpeg($destino);
+
+                // Obtenha as dimensões da imagem original
+                $original_width = imagesx($original_image);
+                $original_height = imagesy($original_image);
+
+                // Calcule a posição central da imagem original
+                $center_x = round($original_width / 2);
+                $center_y = round($original_height / 2);
+
+                // Calcule as coordenadas do ponto superior esquerdo da imagem cortada
+                $top_left_x = $center_x - round($new_width / 2);
+                $top_left_y = $center_y - round($new_height / 2);
+
+                // Crie uma nova imagem com as dimensões desejadas
+                $cropped_image = imagecreatetruecolor($new_width, $new_height);
+
+                // Recorte a imagem original para a nova imagem
+                imagecopyresampled($cropped_image, $original_image, 0, 0, $top_left_x, $top_left_y, $new_width, $new_height, $new_width, $new_height);
+
+                // Salve a nova imagem
+                imagejpeg($cropped_image, $destino);
+
+                // Libere a memória usada pelas imagens
+                imagedestroy($original_image);
+                imagedestroy($cropped_image);
                 
                 
             }
---
+++ View/recortar_imagens.php
@@ -0,0 +1,41 @@
+<?php
+include_once '../Model/Conexao.php';
+
+
+$res=$conexao->query("SELECT * from aluno where aluno.imagem_carteirinha_transporte !=''");
+foreach ($res as $key => $value) {
+
+$imagem_carteirinha_transporte=$value['imagem_carteirinha_transporte'];
+
+$new_width = 900;
+$new_height = 800;
+
+// Carregue a imagem original
+$original_image = imagecreatefromjpeg("imagem_carteirinha_transporte/$imagem_carteirinha_transporte");
+
+// Obtenha as dimensões da imagem original
+$original_width = imagesx($original_image);
+$original_height = imagesy($original_image);
+
+// Calcule a posição central da imagem original
+$center_x = round($original_width / 2);
+$center_y = round($original_height / 2);
+
+// Calcule as coordenadas do ponto superior esquerdo da imagem cortada
+$top_left_x = $center_x - round($new_width / 2);
+$top_left_y = $center_y - round($new_height / 2);
+
+// Crie uma nova imagem com as dimensões desejadas
+$cropped_image = imagecreatetruecolor($new_width, $new_height);
+
+// Recorte a imagem original para a nova imagem
+imagecopyresampled($cropped_image, $original_image, 0, 0, $top_left_x, $top_left_y, $new_width, $new_height, $new_width, $new_height);
+
+// Salve a nova imagem
+imagejpeg($cropped_image, "imagem_carteirinha_transporte/cortada/$imagem_carteirinha_transporte");
+
+// Libere a memória usada pelas imagens
+imagedestroy($original_image);
+imagedestroy($cropped_image);
+
+}
