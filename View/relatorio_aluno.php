3d0ab9ab4597d7f70f185ed222287e08aac6a641
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-04-13 13:47:25 -0300

    teste

--- View/relatorio_aluno.php
+++ View/relatorio_aluno.php
@@ -40,6 +40,69 @@ if (!isset($_SESSION['idfuncionario'])) {
 
  </style>
 
+<style>
+
+.checkbox-btn {
+  display: inline-block;
+  position: relative;
+}
+
+.checkbox-btn__label {
+  display: inline-block;
+  position: relative;
+  font-size: 16px;
+  line-height: 32px;
+  padding-left: 40px;
+  cursor: pointer;
+}
+
+.checkbox-btn__image {
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 32px;
+  height: 32px;
+  background-image: url("imagens/excel.png");
+  background-size: cover;
+  opacity: 0.5;
+}
+
+.checkbox-btn__image::before {
+  content: "";
+  display: inline-block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.checkbox-btn input[type="checkbox"] {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.checkbox-btn input[type="checkbox"]:checked ~ .checkbox-btn__image {
+  opacity: 1;
+}
+</style>
+<script type="text/javascript">
+  const checkboxBtn = document.querySelector('.checkbox-btn input[type="checkbox"]');
+
+  checkboxBtn.addEventListener('change', () => {
+    const checkboxImage = checkboxBtn.parentNode.querySelector('.checkbox-btn__image');
+    if (checkboxBtn.checked) {
+      checkboxImage.style.opacity = 1;
+    } else {
+      checkboxImage.style.opacity = 0.5;
+    }
+  });
+
+</script>
+
+
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script src="ajax.js?<?php echo rand(); ?>"></script>
 
@@ -271,7 +334,19 @@ if (!isset($_SESSION['idfuncionario'])) {
         </div>
 
 
-        <div class="col-sm-12">
+        <div class="col-sm-4">
+          
+          <div class="checkbox-btn">
+            <input type="checkbox" id="opcao1" name="opcoes">
+            <label for="opcao1" class="checkbox-btn__label">
+              <span class="checkbox-btn__image"></span>
+              Baixar em excel?
+            </label>
+          </div>
+
+        </div>
+
+        <div class="col-sm-8">
           <div class="form-group">
           <a style="margin-top: 30PX;" class="btn btn-block btn-primary" onclick="pesquisa_relatorio_filtros()">Pesquisar</a>
           </div>
