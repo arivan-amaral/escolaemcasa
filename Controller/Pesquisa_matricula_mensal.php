a8e54d2d9f509246fa0b556cdeb83daea2eee694
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-04-13 00:41:36 -0300

    teste

--- Controller/Pesquisa_matricula_mensal.php
+++ Controller/Pesquisa_matricula_mensal.php
@@ -123,8 +123,14 @@ if ($_GET['serie'] == 1 ) {
         
 
           $quant_anterior =  $quant_total - $quant_matriculas;
-          
-          $result_por_turma[$serie_id]=$result_por_turma[$serie_id]."<br> <b>$nome_escola</b><br> $nome_turma -  Qnt Anterior = $quant_anterior + Qnt Matriculas novas=$quant_matriculas Total=$quant_total <br>";
+          if ($quant_matriculas>0) {
+            $cor="text-danger";
+          }else{
+            $cor="";
+
+          }
+
+          $result_por_turma[$serie_id]=$result_por_turma[$serie_id]."<br> <b>$nome_escola</b><br> $nome_turma -  Qnt Anterior = $quant_anterior + Qnt Matriculas novas=<b class='$cor'>$quant_matriculas</b> Total=$quant_total <br>";
           
           $array_quant_anterior[$serie_id]=$array_quant_anterior[$serie_id]+$quant_anterior;
           $array_quant_matriculas[$serie_id]=$array_quant_matriculas[$serie_id]+$quant_matriculas;
