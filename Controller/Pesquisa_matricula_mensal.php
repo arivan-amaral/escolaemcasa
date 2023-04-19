bed80eb97d84e02d438e6d26bcedc639ee3075b4
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-04-18 21:28:11 -0300

    ajustes

--- Controller/Pesquisa_matricula_mensal.php
+++ Controller/Pesquisa_matricula_mensal.php
@@ -133,7 +133,7 @@ if ($_GET['serie'] == 1 ) {
 
           }
 
-          $result_por_turma[$serie_id]=$result_por_turma[$serie_id]."<br> <b class='$cor_card'>$nome_escola</b><br> $nome_turma -  Qnt Anterior = $quant_anterior + Qnt Matriculas novas=<b class='$cor'>$quant_matriculas</b> Total=$quant_total <br>";
+          $result_por_turma[$serie_id]=$result_por_turma[$serie_id]."<br> <b class='$cor_card'>$nome_escola</b><br> $nome_turma - Anterior = $quant_anterior + Novas=<b class='$cor'>$quant_matriculas</b> Total=$quant_total <br>";
           
           $array_quant_anterior[$serie_id]=$array_quant_anterior[$serie_id]+$quant_anterior;
           $array_quant_matriculas[$serie_id]=$array_quant_matriculas[$serie_id]+$quant_matriculas;
