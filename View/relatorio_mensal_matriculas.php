710f021f1220cd9027e7fb4e6254313711f4b381
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-04-25 14:06:13 -0300

    ajustes

--- View/relatorio_mensal_matriculas.php
+++ View/relatorio_mensal_matriculas.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION['idcoordenador'])) {
   $idcoordenador=$_SESSION['idfuncionario'];
 
 }
+  $idcoordenador=$_SESSION['idfuncionario'];
   include_once "cabecalho.php";
   include_once "alertas.php";
  
