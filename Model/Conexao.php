b1cbb56dea6d878514b6858678f0f9dc8b96b560
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-05-21 22:34:24 -0300

    AJUSTES

--- Model/Conexao.php
+++ Model/Conexao.php
@@ -3,13 +3,13 @@ try {
 date_default_timezone_set('America/Sao_Paulo');
     
     $server = "localhost";
-    $username = "educ_lem";
+    $username = "root";
      
-     $password = "Ari200120022003_";
+     $password = "MANUTENCAO";
     
     $conexao = new PDO("mysql:host=$server;dbname=educ_lem;charset=utf8", $username, $password);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Falha na conexão:";
+    echo "ESTAMOS EM MANUTENÇÃO";
 }
 ?>
