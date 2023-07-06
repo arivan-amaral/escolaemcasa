73dbbf9af9cbd0dfe769e6453ce191327ef146b9
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-07-05 21:25:41 -0300

    ADD ITEM MENU registro_ligação.php

--- View/menu.php
+++ View/menu.php
@@ -207,7 +207,7 @@
 
               
                <li class='nav-item'>
-               <a href='#' class='nav-link'>
+               <a href='registro_ligação.php' class='nav-link'>
                <i class='far fa-circle nav-icon text-primary'></i>
                <p>Registro de ligação</p>
                </a>
