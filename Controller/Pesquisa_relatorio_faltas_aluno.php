3c3e6f8e35af5840a98f2990f76b8fd88f0103fc
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-07-10 07:43:46 -0300

    ajustes

--- Controller/Pesquisa_relatorio_faltas_aluno.php
+++ Controller/Pesquisa_relatorio_faltas_aluno.php
@@ -10,15 +10,15 @@ $data_inicial=$_GET['data_inicial'];
 $data_final=$_GET['data_final'];
 $escola=$_GET['escola'];
 
-$res=$conexao->query("SELECT aluno.nome AS nome_aluno, COUNT(frequncia.presenca) AS quantidade_faltas
-FROM aluno,ecidade_matricula,frequncia
+$res=$conexao->query("SELECT aluno.nome AS nome_aluno, COUNT(frequencia.presenca) AS quantidade_faltas
+FROM aluno,ecidade_matricula,frequencia
 WHERE 
-aluno.idaluno = frequncia.aluno_id and 
+aluno.idaluno = frequencia.aluno_id and 
 aluno.idaluno = ecidade_matricula.aluno_id and 
-data_frequncia BETWEEN '$data_inicial' AND '$data_final'
+data_frequencia BETWEEN '$data_inicial' AND '$data_final'
 AND ecidade_matricula.matricula_ativa = 'S' and turma_escola= $escola
 GROUP BY aluno.idaluno
-HAVING COUNT(frequncia.presenca) > 0
+HAVING COUNT(frequencia.presenca) > 0
 ORDER BY quantidade_faltas desc"
 );
 foreach ($res as $key => $value) {
