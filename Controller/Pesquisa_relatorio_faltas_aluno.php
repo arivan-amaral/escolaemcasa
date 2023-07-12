9d6f6f4422eef70c968ca4830c186e96eb83dfcb
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-07-11 23:27:47 -0300

    ajustes

--- Controller/Pesquisa_relatorio_faltas_aluno.php
+++ Controller/Pesquisa_relatorio_faltas_aluno.php
@@ -113,7 +113,7 @@ ORDER BY aluno.nome ASC");
 
            foreach ($array_datas as $key => $datas) {
              
-               // if ($faltas_aluno<=$quantidade_falta) {
+               if ($faltas_aluno<=$quantidade_falta) {
                    $res=$conexao->query("SELECT * FROM frequencia WHERE ano_frequencia='$ano_letivo' and
                     data_frequencia ='$datas' and aluno_id=$idaluno and turma_id=$turma_id and escola_id=$escola_id  and  presenca !=1 limit 1 ");
                   
