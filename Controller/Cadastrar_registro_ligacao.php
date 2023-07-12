ff284865e2b6d8fcf80a5c7aec1f07c930f58455
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-07-12 08:46:58 -0300

    ajustes

--- Controller/Cadastrar_registro_ligacao.php
+++ Controller/Cadastrar_registro_ligacao.php
@@ -21,14 +21,14 @@ try{
  
 
     // Preparar e executar o INSERT na tabela
-    $stmt = $conexao->prepare("INSERT INTO sua_tabela (aluno_id, periodo_inicial, periodo_final, quantidade_faltas, funcionario_id, quem_atendeu, descricao_chamada, exitosa, ficai) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt = $conexao->prepare("INSERT INTO busca_ativa (aluno_id, periodo_inicial, periodo_final, quantidade_faltas, funcionario_id, quem_atendeu, descricao_chamada, exitosa, ficai) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
     if ( $stmt->execute(array( $idaluno, $data_inicial, $data_final, $quantidade_faltas,$funcionario_id, $quemAtendeu, $descricaoChamada, $exitosa, $ficai))) {
         $_SESSION['status']=1;
         header("Location:../View/registro_ligacao.php");
     } else {
         $_SESSION['status']=0;
-        header("Location:../View/relatorio_falta.php");
+       header("Location:../View/relatorio_falta.php");
 
     }
 
@@ -36,8 +36,8 @@ try{
 
 } catch (Exception $e) {
             $_SESSION['status']=0;
-        header("Location:../View/relatorio_falta.php");
-        // echo $e;
+       header("Location:../View/relatorio_falta.php");
+       //  echo $e;
 }
 
 
