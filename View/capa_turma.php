154881ac25e68b5cbfdc31dd0ed77eca5c03f654
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-08-16 01:42:41 -0300

    ajustes

--- View/capa_turma.php
+++ View/capa_turma.php
@@ -107,6 +107,8 @@ href="regitro_conteudo_arquivos/colorschememapping.xml">
 
  <body>
  
+gerar pdf
+
  <div class="load"> <i class="fa fa-cog fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> </div>
 
 <div class="content-wrapper" style="min-height: 529px;background-color: white;">
