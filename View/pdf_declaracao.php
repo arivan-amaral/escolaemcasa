0785b8fba4deeb9ed5f7505df15cf272f7d71ba7
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-08-30 13:55:16 -0300

    a

--- View/pdf_declaracao.php
+++ View/pdf_declaracao.php
@@ -1,5 +1,5 @@
  <?php
- session_start() 
+ session_start(); 
 if (!isset($_SESSION['usuariobd'])) {
     // Se não estiver definida, atribui o valor padrão 'educ_lem'
     $_SESSION['usuariobd'] = 'educ_lem';
