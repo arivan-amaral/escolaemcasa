f831436e5fa122601426d7f5ed992ee57efe49f1
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-08-31 09:10:45 -0300

    ajustes

--- View/conteudos_registrados_infantil.php
+++ View/conteudos_registrados_infantil.php
@@ -345,7 +345,7 @@ foreach ($array_datas as $key => $value) {
               $result_funcionario_conteudo= $conexao->query("SELECT * FROM 
           funcionario,conteudo_aula
          where 
-        ( funcionario_id=idfuncionario)and   $idconteudo_prof  limit 10 ");
+        ( funcionario_id=idfuncionario)and   $idconteudo_prof  limit 2 ");
 
         $array_nome_professor = array();
         $conta_cont=0;
