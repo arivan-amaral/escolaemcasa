82ab5fcbfed79dac31a89b866555beb69d9bd549
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-08-31 08:52:43 -0300

    ajustes

--- View/conteudos_registrados_infantil.php
+++ View/conteudos_registrados_infantil.php
@@ -127,8 +127,17 @@
   $disciplinas_regente_abreviacao =array();
   
   $nome_disciplina="";
+  $array_nome_professor= array();
+  $nome_professor= "";
 
   foreach ($res_disciplinas as $key => $value) {
+    $novo_nome_prof=$value['nome_professor'];
+    if (!in_array($novo_nome_prof, $array_nome_professor)) {
+        // Se não existir, adiciona o novo nome ao array
+        $array_nome_professor[] = $novo_nome;
+        $nome_professor.= "$novo_nome<br>";
+
+    }
     $disciplina_id=$value['disciplina_id'];
 
     $nome_disciplina.=$value['nome_disciplina'];
@@ -339,14 +348,17 @@ foreach ($array_datas as $key => $value) {
       mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
       color:black;mso-fareast-language:PT-BR'>
       <?php 
-              $result_funcionario_conteudo= $conexao->query("SELECT * FROM 
-          funcionario,conteudo_aula
-         where 
-        ( funcionario_id=idfuncionario or  professor_id=idfuncionario )and  id =$idconteudo  limit 5 ");
-        foreach ($result_funcionario_conteudo as $key => $value) {
-          $nome_funcionario=$value['nome'];
-          echo "<b>$nome_funcionario</b><br>";
-        }
+        //       $result_funcionario_conteudo= $conexao->query("SELECT * FROM 
+        //   funcionario,conteudo_aula
+        //  where 
+        // ( funcionario_id=idfuncionario or  professor_id=idfuncionario )and disciplina_id =$idconteudo  limit 5 ");
+        // foreach ($result_funcionario_conteudo as $key => $value) {
+        //   $nome_funcionario=$value['nome'];
+        //   echo "<b>$nome_funcionario</b> <br>";
+        // }
+        //
+           echo "<b>$nome_professor</b> <br>";
+        
        ?>
 
 
