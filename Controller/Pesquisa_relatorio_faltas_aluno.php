84acec6d3c44c8e844935e6ca8f41ce52a66bf8f
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2023-09-02 10:10:24 -0300

    ajustes

--- Controller/Pesquisa_relatorio_faltas_aluno.php
+++ Controller/Pesquisa_relatorio_faltas_aluno.php
@@ -1,31 +1,25 @@
-<?php 
-session_start();
+<?php
 set_time_limit(500);
+session_start();
 
-    if (!isset($_SESSION['usuariobd'])) {
-    // Se não estiver definida, atribui o valor padrão 'educ_lem'
-    $_SESSION['usuariobd'] = 'educ_lem';
+if (!isset($_SESSION['usuariobd'])) {
+    $_SESSION['usuariobd'] = 'educ_lem'; // Define um valor padrão se não estiver definido
 }
-$usuariobd=$_SESSION['usuariobd'];
-include_once "../Model/Conexao_".$usuariobd.".php";
-    include_once '../Model/Aluno.php';
-    include_once "Conversao.php";
 
-//     ini_set('display_errors', '1');
-// ini_set('display_startup_errors', '1');
-// error_reporting(E_ALL);
- 
+$usuariobd = $_SESSION['usuariobd'];
+include_once "../Model/Conexao_" . $usuariobd . ".php";
+include_once '../Model/Aluno.php';
+include_once "Conversao.php";
 
 function obterDatasEntrePeriodo($dataInicial, $dataFinal) {
     // Converte as datas para o formato do objeto DateTime
     $dataInicial = new DateTime($dataInicial);
     $dataFinal = new DateTime($dataFinal);
 
-    // Incrementa um dia na data inicial para garantir que ela esteja incluída no resultado
-    //  $dataInicial->modify('-1 day');
+    // Incrementa um dia na data final para garantir que ela esteja incluída no resultado
     $dataFinal->modify('+1 day');
 
-    // Cria um intervalo de datas usando a data inicial, a data final e o passo de 1 dia
+    // Cria um intervalo de datas usando o período entre a data inicial e final
     $intervalo = new DateInterval('P1D');
     $periodo = new DatePeriod($dataInicial, $intervalo, $dataFinal);
 
@@ -40,66 +34,24 @@ function obterDatasEntrePeriodo($dataInicial, $dataFinal) {
     return $datas;
 }
 
- 
-
-    // try {
-    $quantidade_falta = $_GET['falta'];
-
-    $ano_letivo = $_SESSION['ano_letivo'];
-    $idturma = $_GET['idturma'];
-    
-    $campo_turma=($_GET['idturma']);
-    $delimit=",";
-
-    // $turma=explode($delimit,$campo_turma);
-    // $turma_aux="";
 
 
-    
+// Parâmetros da URL
+$quantidade_falta = $_GET['falta'];
+$ano_letivo = $_SESSION['ano_letivo'];
+$idturma = $_GET['idturma'];
+$campo_turma = $_GET['idturma'];
+$delimit = ",";
 
-    $idescola = $_GET['idescola'];
-    $data_inicial = $_GET['data_inicial'];
-    $data_final = $_GET['data_final'];
+$idescola = ($_GET['idescola'] === 'Todas') ? ' and escola.idescola > 0 ' : ' and escola.idescola = ' . $_GET['idescola'];
 
-     $data_inicio = new DateTime("$data_inicial");
-     $data_fim = new DateTime("$data_final");
-     $dateInterval = $data_inicio->diff($data_fim);
-     $total= $dateInterval->days;
-      
+$idturma = ($idturma === '' || $idturma === 'Todas') ? ' and ecidade_matricula.turma_id > 0 ' : ' and ecidade_matricula.turma_id IN(' . $campo_turma . ')';
 
- if ($idescola =='Todas') {
-	$idescola=" and escola.idescola >0 ";
-}else{
-	$idescola=" and escola.idescola = $idescola ";
+// Função para obter datas entre um período
+$array_datas = obterDatasEntrePeriodo($_GET['data_inicial'], $_GET['data_final']);
 
-}
-
-
- 
-if ($idturma =='' || $idturma =='Todas') {
-    $idturma=" and ecidade_matricula.turma_id >0 ";
-}else{
-    
-    $idturma=" and ecidade_matricula.turma_id IN($campo_turma) ";
-    
-
-}
-
-
-// if ($idturma =='Todas') {
-// 	$idturma=" and turma.idturma >0 ";
-// }else{
-// 		$idturma=" and turma.idturma = $idturma ";
-
-
-// }
-
- 
-       
-$array_datas = obterDatasEntrePeriodo($data_inicial, $data_final);
-
- 
-    $resultado=$conexao->query("
+// Consulta SQL
+$resultado = $conexao->query("
     SELECT
     escola.nome_escola,
     aluno.aluno_transpublico, 
@@ -130,135 +82,76 @@ WHERE
  $idescola $idturma
 ORDER BY escola.nome_escola, turma.nome_turma, aluno.nome ASC");
 
+// Tabela HTML
+$result = "
+<table class='table table-bordered table-striped'>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Aluno</th>
+            <th>Ação</th>
+            <th>Faltas Consecutivas</th>
+            <th>Total no período</th>
+        </tr>
+    </thead>
+    <tbody>";
 
+$conta_aluno = 1;
 
+foreach ($resultado as $key => $value) {
+    $nome_escola = $value['nome_escola'];
+    $nome_aluno = $value['nome_aluno'];
+    $whatsapp = $value['whatsapp'];
+    $whatsapp_responsavel = $value['whatsapp_responsavel'];
+    $nome_turma = $value['nome_turma'];
+    $id = $value['idaluno'];
+    $idaluno = $value['idaluno'];
+    $status_aluno = $value['status_aluno'];
+    $email = $value['email'];
+    $senha = $value['senha'];
+    $matricula_aluno = $value['matricula'];
+    $turma_id = $value['turma_id'];
+    $escola_id = $value['escola_id'];
 
-    $result="
-  <table class='table table-bordered table-striped'>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Aluno</th>
-                        <th>Ação</th>
-                        <th>Faltas Consecutivas</th>
-                        <th>Total no período</th>
-                                            
-                    </tr>
-                </thead>
-                <tbody>";
-    $conta_aluno=1;
+    $faltas_aluno = 0;
+    $total_faltas_aluno = 0;
 
-     foreach ($resultado as $key => $value) {
+    foreach ($array_datas as $key => $datas) {
+        $res_faltas = $conexao->query("SELECT * FROM frequencia WHERE ano_frequencia='$ano_letivo' and
+            data_frequencia ='$datas' and aluno_id=$idaluno and turma_id=$turma_id and escola_id=$escola_id  and  presenca !=1 limit 1 ");
 
-       $nome_escola=($value['nome_escola']);
-       $nome_aluno=($value['nome_aluno']);
-       $whatsapp=($value['whatsapp']);
-       $whatsapp_responsavel=($value['whatsapp_responsavel']);
-       $nome_turma=($value['nome_turma']);
-       $id=$value['idaluno'];
-       $idaluno=$value['idaluno'];
-       $status_aluno=$value['status_aluno'];
-       $email=$value['email'];
-       $senha=$value['senha'];
-       $matricula_aluno=$value['matricula'];
-       $turma_id=$value['turma_id'];
-       $escola_id=$value['escola_id'];
-
-       
-
-        $faltas_aluno=0;
-        $total_faltas_aluno=0;
-
-   // echo "($faltas_aluno<=$quantidade_falta)";
-
-           foreach ($array_datas as $key => $datas) {
-          // echo "w$faltas_aluno <br>";
-               // if ($faltas_aluno<=$quantidade_falta) {
-                  
-                   $res_faltas=$conexao->query("SELECT * FROM frequencia WHERE ano_frequencia='$ano_letivo' and
-                    data_frequencia ='$datas' and aluno_id=$idaluno and turma_id=$turma_id and escola_id=$escola_id  and  presenca !=1 limit 1 ");
-
-                // if ($_SESSION['nivel_acesso_id'] ==100) {
-                  
-                        
-                   // $result.="SELECT * FROM frequencia WHERE ano_frequencia='$ano_letivo' and
-                   // data_frequencia ='$datas' and aluno_id=$idaluno and turma_id=$turma_id and escola_id=$escola_id  and  presenca !=1 limit 1; <br>";
-
-                // }
-                 // $conta_faltas=0;
-                 //   foreach ($res_faltas as $key => $value) {
-                 //       $faltas_aluno++;
-                 //        $conta_faltas++;
-
-                 //      $total_faltas_aluno++;
-                 //   }
-                 //   if ($conta_faltas==0) {
-                 //     $faltas_aluno=0;
-
-                 //   }
-
-                   if (count($res_faltas->fetchAll())>0) {
-                      $faltas_aluno++;
-                      $total_faltas_aluno++;
-                   }
-
-                   else if ($faltas_aluno < $quantidade_falta ) {
-                     
-                        $faltas_aluno=0;
-                   }
-               // }
-
-  
-       } 
-
-
-
-     if ($faltas_aluno>=$quantidade_falta) {
-
-            $result.="
-               <tr> 
-                   <td>
-                        
-                 $conta_aluno 
-                    
-                  </td> <td>
-                        
-                 $id - $nome_aluno
-                 <br>
-                 $nome_escola <br>
-                 $nome_turma <br>
-                 <b>CONTATO:</b><br>
-                 <b class='text-primary'>$whatsapp</b>/<br>
-                 <b class='text-primary'>$whatsapp_responsavel</b>/<br>
-                    
-                  </td>
-                  <td> <a href='cadastrar_registro_ligacao.php?nome_aluno=$nome_aluno&data_inicial=$data_inicial&data_final=$data_final&idaluno=$idaluno&quantidade_falta=$total_faltas_aluno&escola_id=$escola_id&turma_id=$turma_id' class='btn btn-success' target='_blank' >Registrar chamada</a> 
-                  </td>
-                  <td>
-                    $quantidade_falta ou +
-                  </td>  
-                   <td>
-                    $total_faltas_aluno
-                  </td>
-               ";
-    $conta_aluno++;
-     
-     }
-
-    $faltas_aluno=0;
+        if (count($res_faltas->fetchAll()) > 0) {
+            $faltas_aluno++;
+            $total_faltas_aluno++;
+        } elseif ($faltas_aluno < $quantidade_falta) {
+            $faltas_aluno = 0;
+        }
     }
 
+    if ($faltas_aluno >= $quantidade_falta) {
+        $result .= "
+        <tr> 
+            <td>$conta_aluno</td>
+            <td>
+                $id - $nome_aluno<br>
+                $nome_escola<br>
+                $nome_turma<br>
+                <b>CONTATO:</b><br>
+                <b class='text-primary'>$whatsapp</b>/<br>
+                <b class='text-primary'>$whatsapp_responsavel</b>/<br>
+            </td>
+            <td>
+                <a href='cadastrar_registro_ligacao.php?nome_aluno=$nome_aluno&data_inicial=$data_inicial&data_final=$data_final&idaluno=$idaluno&quantidade_falta=$total_faltas_aluno&escola_id=$escola_id&turma_id=$turma_id' class='btn btn-success' target='_blank'>Registrar chamada</a>
+            </td>
+            <td>$quantidade_falta ou +</td>  
+            <td>$total_faltas_aluno</td>
+        </tr>";
 
+        $conta_aluno++;
+    }
 
+    $faltas_aluno = 0;
+}
 
-    
-    echo "$result </tbody></table>";
-     
-     // } catch (Exception $e) {
-     //    echo $e;
-     // }
-
-
-     
-
-    ?>
+echo "$result </tbody></table>";
+?>
