a5931d4acedde27fb5bcf8a9776f53af755c5c3a
pablo <wademcfild@gmail.com>
2023-11-08 09:46:47 -0300

    ajustes

--- View/capa_turma.php
+++ View/capa_turma.php
@@ -145,10 +145,10 @@ $nome_turma = $_POST['nome_turma'];
   <br>
   <br>
 
-  <div class="excel-button-div">
+
     <center><button class="excel-button">Gerar Excel</button>
     </center>
-  </div>
+
   <div class="load"> <i class="fa fa-cog fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> </div>
 
   <div class="content-wrapper" style="min-height: 529px;background-color: white;">
