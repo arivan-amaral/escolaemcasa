1ebb12a94597e69d4ea2042195105b1c125a76a0
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2024-02-01 16:51:52 -0300

    ajustes

--- View/ata_resultado_final_funcao.php
+++ View/ata_resultado_final_funcao.php
@@ -368,15 +368,7 @@ if ($media_fora_rede==0) {
       
       if ($_SESSION['idcoordenador']==176) {
        
-        // echo "SELECT
-        // avaliacao,periodo_id,escola_id,nota
-        //  FROM nota_parecer WHERE
-        // escola_id=$idescola and
-        // turma_id=$idturma and
-        // disciplina_id=$iddisciplina and 
-        // periodo_id=1 and aluno_id=$idaluno  group by avaliacao,periodo_id,nota <br><br>";
-        // 
-        // 
+
         echo  number_format($media, 1, '.', ',');
 
       }else{
@@ -400,15 +392,18 @@ if ($media_fora_rede==0) {
           $media_aprovacao="Não";
           
           if ($_SESSION['idcoordenador']==176) {
-             //echo"($nota_tri_3+$nota_tri_2+$nota_tri_1)/3";
+             echo"
+             ($nota_tri_3+$nota_tri_2+$nota_tri_1)/3 
+             <br>
+             ($nota_tri_3,$nota_rp_3,$nota_av3_3) ";
           }
         
 
         if ($_SESSION['idcoordenador']==176) {
-          echo "($nota_tri_1+$nota_tri_2+$nota_tri_3)/3 ";
+          //echo "($nota_tri_1+$nota_tri_2+$nota_tri_3)/3 ";
         }
 
-          echo "". number_format($media, 1, '.', ',') ."";
+          //echo "". number_format($media, 1, '.', ',') ."";
 
       }
 
