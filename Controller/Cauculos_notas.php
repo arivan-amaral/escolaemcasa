04594a6359146d0e0c8c520cae9f06b031ec1371
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2024-02-01 16:44:03 -0300

    teste

--- Controller/Cauculos_notas.php
+++ Controller/Cauculos_notas.php
@@ -1,4 +1,5 @@
 <?php 
+
 	function calculos_media_notas($nota,$nota_rp,$nota_av3){
 		$media_nota=0;
 		if (!is_numeric($nota)) {
@@ -11,8 +12,9 @@
 			$nota_av3=0;
 		}
 		
-		if ( $nota!='' && $nota_rp >$nota_av3) {
-	       $media_nota=($nota-$nota_av3)+$nota_rp;
+		if ( $nota!='' && $nota_rp > $nota_av3) {
+
+	       $media_nota="recarregue a pag:".($nota-$nota_av3)+$nota_rp;
 	     }else{
 	     	$media_nota=$nota;
 	     }
