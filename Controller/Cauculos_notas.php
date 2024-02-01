44f7cf2eed3d3014e9a573c200f1b85530f01e21
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2024-02-01 16:45:39 -0300

    teste

--- Controller/Cauculos_notas.php
+++ Controller/Cauculos_notas.php
@@ -15,8 +15,7 @@
 		if ( $nota!='' && $nota_rp > $nota_av3) {
 
 	       $media_nota=($nota-$nota_av3)+$nota_rp;
-         echo "recarregue a pag:";
-	     }else{
+         echo "rec: ($nota-$nota_av3)+$nota_rp";
 	     	$media_nota=$nota;
 	     }
      return $media_nota;
