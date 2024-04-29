ec3846dd932e7202e4078644e400b3ab20a48213
Pablo Silva Carrilho de Resende <126430073+PabloResende@users.noreply.github.com>
2024-04-29 09:40:19 -0300

    impede que o mesmo cpf ceja cadastrado

--- Controller/Cadastro_professor.php
+++ Controller/Cadastro_professor.php
@@ -1,51 +1,45 @@
-<?php session_start();
+<?php 
+session_start();
+
 if (!isset($_SESSION['usuariobd'])) {
     // Se não estiver definida, atribui o valor padrão 'educ_lem'
     $_SESSION['usuariobd'] = 'educ_lem';
 }
-$usuariobd=$_SESSION['usuariobd'];
+
+$usuariobd = $_SESSION['usuariobd'];
 include_once "../Model/Conexao_".$usuariobd.".php";
 include_once '../Model/Professor.php';
 include_once 'Conversao.php';
 
- try {
+try {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $whatsapp = "55" . $_POST['whatsapp'];
+    $whatsapp = str_replace(' ', '', $whatsapp);
+    $whatsapp = str_replace('(', '', $whatsapp);
+    $whatsapp = str_replace(')', '', $whatsapp);
+    $whatsapp = str_replace('-', '', $whatsapp);
+    $cpf = converte_telefone($_POST['cpf']);
 
- 	 $nome=$_POST['nome'];
- 	 $email=$_POST['email'];
- 	 $senha=$_POST['senha'];
- 	 $whatsapp="55".$_POST['whatsapp'];
+    $sql = "SELECT cpf FROM funcionario WHERE cpf = :cpf";
+    $stmt = $conexao->prepare($sql);
+    $stmt->bindValue(':cpf', $cpf);
+    $stmt->execute();
+    $resultado = $stmt->fetch();
 
- 	 $whatsapp= str_replace(' ', '', $whatsapp);
- 	 $whatsapp= str_replace('(', '', $whatsapp);
- 	 $whatsapp= str_replace(')', '', $whatsapp);
- 	 $whatsapp= str_replace('-', '', $whatsapp);
- 	 $cpf=converte_telefone($_POST['cpf']);
- 	 // echo "$nome <br>";
- 	 // echo "$email <br>";
- 	 // echo "$senha <br>";
- 	 // echo "$whatsapp <br>";
-	$funcao='';
+    if ($resultado) {
+        $_SESSION['status'] = 0;
+        header("location:../View/cadastro_professor.php?status=cpf_existente");
+        exit(); 
+    }
 
-if ($_POST['sexo']=="Masculino") {
-	$funcao='Professor';
-}else {
-	$funcao='Professora';
-	
+    $funcao = ($_POST['sexo'] == "Masculino") ? 'Professor' : 'Professora';
+    cadastro_professor($conexao, $nome, $email, $funcao, $whatsapp, $senha, $cpf);
+    $_SESSION['status'] = 1;
+    header("location:../View/cadastro_professor.php?status=1");
+} catch (Exception $e) {
+    $_SESSION['status'] = 0;
+    header("location:../View/cadastro_professor.php?status=0");
 }
- 	 cadastro_professor($conexao,$nome, $email, $funcao,$whatsapp, $senha,$cpf);
-
- 	 $_SESSION['status']=1;
-
-
- 	 
- 	 
- 	 header("location:../View/cadastro_professor.php?status=1");
-
- } catch (Exception $e) {
- 	 $_SESSION['status']=0;
- 	  header("location:../View/cadastro_professor.php?status=0");
- }
-
-
-
 ?>
