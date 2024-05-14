d70e97c9a271fa26a45cbdda7f8b537cb250300a
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2024-05-14 11:01:20 -0300

    ajustes

--- View/menu_disciplina_regente.php
+++ View/menu_disciplina_regente.php
@@ -247,16 +247,16 @@ echo " <a class='btn btn-secondary btn-block btn-flat' href='boletim.php?idescol
                 </a>";
 if ($idserie < 8 || ($seguimento != '' && $seguimento < 3) || ($idserie == 16 && $seguimento < 3)) {
   echo "
-                      <a   href='impressao_diario_frequencia.php?iddisciplina=1000&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=1' class='btn btn-secondary btn-block btn-flat' target='_blank'>
+                      <a   href='impressao_diario_frequencia.php?iddisciplina=$iddisciplina&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=1' class='btn btn-secondary btn-block btn-flat' target='_blank'>
                       <i class='fa fa-calendar'></i> 
                       FICHA DE RENDIMENTO TRI I
                       </a> 
 
-                      <a   href='impressao_diario_frequencia.php?iddisciplina=1000&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=2' class='btn btn-secondary btn-block btn-flat' target='_blank'>
+                      <a   href='impressao_diario_frequencia.php?iddisciplina=$iddisciplina&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=2' class='btn btn-secondary btn-block btn-flat' target='_blank'>
                       <i class='fa fa-calendar'></i> 
                       FICHA DE RENDIMENTO TRI II
                       </a>   
-                      <a   href='impressao_diario_frequencia.php?iddisciplina=1000&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=3' class='btn btn-secondary btn-block btn-flat' target='_blank'>
+                      <a   href='impressao_diario_frequencia.php?iddisciplina=$iddisciplina&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=3' class='btn btn-secondary btn-block btn-flat' target='_blank'>
                       <i class='fa fa-calendar'></i> 
                       FICHA DE RENDIMENTO TRI III
                       </a>  ";
