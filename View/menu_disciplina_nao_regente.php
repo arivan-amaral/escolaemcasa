0457108c4acf025dd697d0f8b0ce49d70d03f137
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2024-05-14 10:59:31 -0300

    ajustes

--- View/menu_disciplina_nao_regente.php
+++ View/menu_disciplina_nao_regente.php
@@ -253,16 +253,16 @@
                                   if ( $idserie<8 || ($seguimento !='' && $seguimento <3) || ( $idserie==16 && $seguimento <3)) {
 
                                                     echo "
-                                                      <a   href='impressao_diario_frequencia.php?iddisciplina=1000&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=1' class='btn btn-secondary btn-block btn-flat' target='_blank'>
+                                                      <a   href='impressao_diario_frequencia.php?iddisciplina=$iddisciplina&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=1' class='btn btn-secondary btn-block btn-flat' target='_blank'>
                                                       <i class='fa fa-calendar'></i> 
                                                       FICHA DE RENDIMENTO TRI I
                                                       </a> 
 
-                                                      <a   href='impressao_diario_frequencia.php?iddisciplina=1000&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=2' class='btn btn-secondary btn-block btn-flat' target='_blank'>
+                                                      <a   href='impressao_diario_frequencia.php?iddisciplina=$iddisciplina&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=2' class='btn btn-secondary btn-block btn-flat' target='_blank'>
                                                       <i class='fa fa-calendar'></i> 
                                                       FICHA DE RENDIMENTO TRI II
                                                       </a>   
-                                                      <a   href='impressao_diario_frequencia.php?iddisciplina=1000&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=3' class='btn btn-secondary btn-block btn-flat' target='_blank'>
+                                                      <a   href='impressao_diario_frequencia.php?iddisciplina=$iddisciplina&idturma=$idturma&idescola=$idescola&idserie=$idserie&periodo_id=3' class='btn btn-secondary btn-block btn-flat' target='_blank'>
                                                       <i class='fa fa-calendar'></i> 
                                                       FICHA DE RENDIMENTO TRI III
                                                       </a>  "; 
