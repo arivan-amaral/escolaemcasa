13a72c06cda4fede8fccab7d685242c7d3a7f97b
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2024-07-18 14:46:45 -0300

    AJUSTES

--- Controller/Login.php
+++ Controller/Login.php
@@ -1,4 +1,12 @@
 <?php
+
+
+  echo "<H1>ESTAMOS EM MANUTENÇÃO E ATUALIZAÇÃO </H1>";
+  echo "<img src='../View/imagens/estamos-em-manutencao.png'>";
+
+
+
+  exit();
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
