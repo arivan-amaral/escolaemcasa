1067e0eb1cedc9d4e2890436bf2204c6a0a57a18
Pablo Silva Carrilho de Resende <126430073+PabloResende@users.noreply.github.com>
2024-08-28 10:21:02 -0300

    ajustes

--- Controller/Cadastrar_conteudo.php
+++ Controller/Cadastrar_conteudo.php
@@ -90,7 +90,7 @@ if (in_array($professor_id, $array_liberados) || $_SESSION["cargo"] !="Professor
             if (isset($_POST["descricao$campo_origem_conteudo"])) {
              $descricao=escape_mimic($_POST["descricao$campo_origem_conteudo"]);
             }
-            if (strlen($descricao) < 24) {
+            if (strlen($descricao) < 10) {
                 $_SESSION['status'] = 0;
                 $_SESSION['mensagem'] = 'Conteúdo não atingiu a quantidade mínima de caracteres!';
                 header("location: ../View/cadastrar_conteudo.php?$url_get");
