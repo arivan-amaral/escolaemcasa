ea93d391fccbb8cfff03eb1b6743b047b90af901
Pablo Silva Carrilho de Resende <126430073+PabloResende@users.noreply.github.com>
2024-10-16 15:43:33 -0300

    sanitização do código + logs

--- Controller/Cadastrar_conteudo.php
+++ Controller/Cadastrar_conteudo.php
@@ -1,155 +1,109 @@
 <?php
-	error_reporting(E_ALL);
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
-    session_start();
+session_start();
 
-    if (!isset($_SESSION['usuariobd'])) {
-    // Se não estiver definida, atribui o valor padrão 'educ_lem'
+if (!isset($_SESSION['usuariobd'])) {
     $_SESSION['usuariobd'] = 'educ_lem';
 }
-$usuariobd=$_SESSION['usuariobd'];
-include_once "../Model/Conexao_".$usuariobd.".php";
-    include("../Model/Aluno.php");
-    include("../Model/Escola.php");
-    include("Conversao.php");
-    include("Liberador.php");
-
+$usuariobd = $_SESSION['usuariobd'];
+include_once "../Model/Conexao_" . $usuariobd . ".php";
+include("../Model/Aluno.php");
+include("../Model/Escola.php");
+include("Conversao.php");
+include("Liberador.php");
 
 try {
+    $idfuncionario = $_SESSION['idfuncionario'];
 
-    // $professor_id=$_SESSION['idfuncionario'];
-
-if (isset($_POST['idprofessor'])) {
-   $professor_id= $_POST['idprofessor'];
-    $idfuncionario=$_SESSION['idfuncionario'];
-}
-
-  $idfuncionario=$_SESSION['idfuncionario'];
-
-
-    // $idescola=$_POST['idescola'];
-   	// $idturma=$_POST['idturma'];
-    // $iddisciplina=$_POST['iddisciplina'];
-
-    $data=$_POST['data_frequencia'];
-    $ano_conteudo=$_SESSION['ano_letivo'];
-    $descricao="";
-    // if (isset($_POST['descricao'])) {
-    //  $descricao=escape_mimic($_POST['descricao']);
-    // }
-
-    $aula=$_POST['aula'];
-    $url_get=$_POST['url_get'];
- ##################### FUNÇÃO VERIFICA BLOQUEIO DE PROFESSOR ##################
-    $res_bloqueio=listar_calendario_por_data($conexao,$data);
-   
-    $idcalendario=0;
-    foreach ($res_bloqueio as $key => $value) {
-        $idcalendario=$value['idcalendario'];
-        break;
+    if (isset($_POST['idprofessor'])) {
+        $professor_id = $_POST['idprofessor'];
     }
-    $verificar_bloqueio=$conexao->query("SELECT * from bloquear_acesso  where funcionario_id = $professor_id and calendario_letivo_id=$idcalendario and status=1
-      ");;
-    $conta_bloqueio=0;
 
-    foreach ($verificar_bloqueio as $key => $value) {
-       $conta_bloqueio=1;
-       break;
+    $data = $_POST['data_frequencia'];
+    $ano_conteudo = $_SESSION['ano_letivo'];
+    $aula = $_POST['aula'];
+    $url_get = $_POST['url_get'];
+
+    // Verificação para garantir que o campo excluir_conteudo esteja sendo recebido
+    if (isset($_POST['excluir_conteudo']) && $_POST['excluir_conteudo'] == 'true') {
+        $excluir_conteudo = true;
+    } else {
+        $excluir_conteudo = false;  // Valor padrão caso não tenha sido enviado
     }
-    // echo "$idcalendario = $conta_bloqueio | SELECT * from bloquear_acesso  where funcionario_id = $professor_id and calendario_letivo_id=$idcalendario and status=1";
-    
-    // die();
-$mes = date("m", strtotime($data));
-
-if (in_array($professor_id, $array_liberados) || $_SESSION["cargo"] !="Professor" ) {
-    // code...
-}elseif ($conta_bloqueio>0 || $mes==06 || $mes ==07 ) {
-  
-        // $_SESSION['status']=2;
-        // $_SESSION['mensagem']='BLOQUEADO PARA PROFESSOR!';
-        // $fallback = '../View/index.php';
-        // $anterior = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $fallback;
-        //  header("location: {$anterior}");
-        // exit();
-}
-      
-
-    ##################### FIM FUNÇÃO VERIFICA BLOQUEIO DE PROFESSOR ##################
-
 
     foreach ($_POST['escola_turma_disciplina'] as $key => $value) {
+        $array_url = explode('-', $_POST['escola_turma_disciplina'][$key]);
+        $idescola = $array_url[0];
+        $idturma = $array_url[1];
+        $iddisciplina = $array_url[2];
+        $idserie = $array_url[3];
+        $campo_origem_conteudo = $idescola . $idturma . $iddisciplina . $idserie;
+        $quantidade_aula = $_POST["quantidade_aula$campo_origem_conteudo"];
 
-        $array_url=explode('-',$_POST['escola_turma_disciplina'][$key]);
-        $idescola=$array_url[0];
-        $idturma=$array_url[1];
-        $iddisciplina=$array_url[2];
-        $idserie=$array_url[3];
-        $campo_origem_conteudo=$idescola."".$idturma."".$iddisciplina."".$idserie;
-        $quantidade_aula=($_POST["quantidade_aula$campo_origem_conteudo"]);
+        if (isset($_POST["descricao$campo_origem_conteudo"])) {
+            $descricao = escape_mimic($_POST["descricao$campo_origem_conteudo"]);
+        }
 
-            if (isset($_POST["descricao$campo_origem_conteudo"])) {
-             $descricao=escape_mimic($_POST["descricao$campo_origem_conteudo"]);
-            }
-            // if (strlen($descricao) < 10) {
-            //     $_SESSION['status'] = 0;
-            //     $_SESSION['mensagem'] = 'Conteúdo não atingiu a quantidade mínima de caracteres!';
-            //     header("location: ../View/cadastrar_conteudo.php?$url_get");
-            //     exit();
-            // }
-            
-            
-            
-        //limpar_cadastro_frequencia($conexao,$idescola,$idturma,$iddisciplina,$professor_id,$data,$aula);
-       
-        // $res_pes_cont_aluno_trasf=pesquisa_conteudo_aula($conexao, $iddisciplina, $idturma, $idescola, $data,$aula);
-        // $idconteudo="";
-        // foreach ($res_pes_cont_aluno_trasf as $key => $value) {
-        //     $idconteudo=$value['id'];
-        // }    
-
-        $res_pes_cont_aluno_trasf=pesquisa_conteudo_aula($conexao, $iddisciplina, $idturma, $idescola, $data,$aula);
-        $idconteudo="";
-        $conteudo_aula_id="";
-
+        $res_pes_cont_aluno_trasf = pesquisa_conteudo_aula($conexao, $iddisciplina, $idturma, $idescola, $data, $aula);
+        $idconteudo = "";
         foreach ($res_pes_cont_aluno_trasf as $key => $value) {
-            $idconteudo=$value['id'];
+            $idconteudo = $value['id'];
 
+            // Verifica se a exclusão foi solicitada
+            if ($excluir_conteudo) {
+                excluir_conteudo_aula($conexao, $idconteudo);
 
-            editar_conteudo_aula($conexao,$descricao, $idconteudo,$quantidade_aula);
-            $conteudo_aula_id=$idconteudo;
+                // Verifica se a exclusão ocorreu e registra o log
+                if ($idconteudo) {
+                    $acao = 'Conteúdo excluído';
+                    registrarLog($conexao, $idfuncionario, $acao);
+                }
+            } else {
+                // Edição de conteúdo
+                editar_conteudo_aula($conexao, $descricao, $idconteudo, $quantidade_aula);
 
+                // Registrar log de edição
+                $acao = 'Edição de conteúdo existente';
+                registrarLog($conexao, $idfuncionario, $acao);
+            }
         }
 
- 
-        if ($idconteudo=="") {
-            
-            cadastro_conteudo_aula($conexao,$descricao, $iddisciplina, $idturma, $idescola, $professor_id, $data,$aula,$ano_conteudo,$quantidade_aula, $idfuncionario);
-            $conteudo_aula_id= $conexao->lastInsertId();
+        if ($idconteudo == "") {
+            // Cadastro de novo conteúdo
+            cadastro_conteudo_aula($conexao, $descricao, $iddisciplina, $idturma, $idescola, $professor_id, $data, $aula, $ano_conteudo, $quantidade_aula, $idfuncionario);
+            $conteudo_aula_id = $conexao->lastInsertId();
+
+            // Registrar log de cadastro
+            $acao = 'Cadastro de conteúdo';
+            registrarLog($conexao, $idfuncionario, $acao);
         }
-
-        //echo "$iddisciplina <br>";
-
-        // arivan 17/09/2021
-
-        // foreach ($_POST['aluno_id'] as $key => $value) {
-        //     $aluno_id=$_POST['aluno_id'][$key];
-        //     $presenca=0;
-
-        //     if (isset($_POST["presenca$aluno_id"])) {
-        //         $presenca=1;
-        //     }
-        //     cadastro_frequencia($conexao,$idescola,$idturma,$iddisciplina,$professor_id,$aluno_id,$data,$conteudo_aula_id,$presenca,$aula);
-        // }
     }
-            $_SESSION['status']=1;
-            header("location: ../View/cadastrar_conteudo.php?$url_get");
-        } catch (Exception $e) {
-            $_SESSION['status']=0;
-            $_SESSION['mensagem']='Alguma coisa deu errado!';
-            $_SESSION['erro_sql']=$e;
-           echo "$e";
-          //header("location: ../View/cadastrar_conteudo.php?$url_get");
 
-         }
+    $_SESSION['status'] = 1;
+    header("location: ../View/cadastrar_conteudo.php?$url_get");
+} catch (Exception $e) {
+    $_SESSION['status'] = 0;
+    $_SESSION['mensagem'] = 'Alguma coisa deu errado!';
+    $_SESSION['erro_sql'] = $e;
+    echo $e;
+}
 
-?>
+// Função para registrar o log de ações
+function registrarLog($conexao, $funcionario_id, $acao) {
+    $data_hora = date("Y-m-d H:i:s");
+    $sql = "INSERT INTO logs (data_hora, acao, funcionario_id) VALUES (:data_hora, :acao, :funcionario_id)";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute([
+        ':data_hora' => $data_hora,
+        ':acao' => $acao,
+        ':funcionario_id' => $funcionario_id,
+    ]);
+}
+
+// Função para excluir o conteúdo da aula
+function excluir_conteudo_aula($conexao, $idconteudo) {
+    $sql = "DELETE FROM conteudo_aula WHERE id = :idconteudo";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute([':idconteudo' => $idconteudo]);
+}
