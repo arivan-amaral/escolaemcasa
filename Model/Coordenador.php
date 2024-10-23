0b7e35b9c4394ca7764f635e75193622cb01b1aa
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2024-10-23 10:48:09 -0300

    ORDENANDO EM ORDEM ALFABETICA

--- Model/Coordenador.php
+++ Model/Coordenador.php
@@ -31,7 +31,7 @@
 
 	function escola_associada($conexao,$idfuncionario){
     	$result=$conexao->query("SELECT * FROM funcionario,relacionamento_funcionario_escola,escola WHERE relacionamento_funcionario_escola.escola_id =escola.idescola and relacionamento_funcionario_escola.funcionario_id = funcionario.idfuncionario  AND  
-			idfuncionario=$idfuncionario");
+			idfuncionario=$idfuncionario ORDER BY escola.nome_escola ASC ");
     	return $result ;
 	}	
 
