575a719a34727e35183f208d9e8ccea27873e1d7
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-01-11 02:37:19 -0300

    conexoes

--- Model/Conexao_aluno.php
+++ Model/Conexao_aluno.php
@@ -4,8 +4,8 @@ date_default_timezone_set('America/Sao_Paulo');
     
     $server = "localhost";
     
-    $username = "educ_lem_producao";
-    $dbname = "educ_lem_producao";
+    $username = "educ_lem"; //usuario do banco 
+    $dbname = "educ_lem_producao";//nome da base de dados
      $password = "Ari200120022003_";
     
     $conexao = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8", $username, $password);
