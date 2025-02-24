45e66e425d4d4dc7242ecd643d1f7bea4159d036
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-02-24 09:18:19 -0300

    declaração

--- View/pdf_declaracao.php
+++ View/pdf_declaracao.php
@@ -39,7 +39,7 @@ $result_ecidade_matricula=$conexao->query("SELECT
              ecidade_matricula.calendario_ano = $ano_letivo and 
              ecidade_matricula.turma_id = turma.idturma and 
              ecidade_matricula.turma_escola = escola.idescola and 
-             ecidade_matricula.matricula_ativa ='S' and turma.serie_id !=17
+             ecidade_matricula.matricula_ativa ='S'
              ORDER by ecidade_matricula.calendario_ano desc");
               $nome_escola="";
               $nome_turma="";
