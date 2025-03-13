de775850567883474fc16c471576c9b810743267
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-03-13 18:45:02 -0300

    ajustes

--- Controller/Listar_chamados.php
+++ Controller/Listar_chamados.php
@@ -13,7 +13,7 @@ try {
     $status = $_GET['status'];
 
     $result = "";
-      $res_resolvidos =listar_chamados($conexao,$setor_id, $status); 
+      $res_resolvidos =listar_chamados($conexao,$setor, $status); 
 
           foreach ($res_resolvidos as $key => $value) {
             $id_chamada = $value['id'];
