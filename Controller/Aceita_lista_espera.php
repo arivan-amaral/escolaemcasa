c419517053de73deee5caf54b649c68472c7cc8f
arivan-amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-03-24 00:01:02 -0300

    teste

--- Controller/Aceita_lista_espera.php
+++ Controller/Aceita_lista_espera.php
@@ -5,14 +5,19 @@
     $_SESSION['usuariobd'] = 'educ_lem';
 }
 $usuariobd=$_SESSION['usuariobd'];
+    $idfuncionario = $_SESSION['idfuncionario'];
+
 include_once "../Model/Conexao_".$usuariobd.".php";
  include_once '../Model/Aluno.php';
  include_once 'Conversao.php';
+ include_once '../Model/Escola.php';
  
 try {
   $id=$_GET['id'];
   $status=$_GET['status'];
+  $acao="Status lista de espera alterado status para $status pelo usuário $idfuncionario";
     aceitar_lista_espera($conexao,$id,$status);
+    registrarLog($conexao, $idfuncionario, $acao);
 
  
 } catch (Exception $e) {
