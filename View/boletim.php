18f5bb392783c13d7cf39807bb517a904ae38423
Arivan Amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-05-15 10:52:09 -0300

    ajustes

--- View/boletim.php
+++ View/boletim.php
@@ -2,9 +2,9 @@
 
 set_time_limit(0);
 session_start();
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
   include"../Controller/Conversao.php";
   include"../Model/Conexao.php";
   include"../Model/Aluno.php";
