1fbb9b3637dc71268d240c7e1d07814282b4a398
Arivan Amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-07-23 16:06:29 -0300

    ajustes

--- Model/Aluno.php
+++ Model/Aluno.php
@@ -158,7 +158,7 @@ function excluir_questao($conexao, $idaluno) {
 function verificar_aluno_na_turna_rematricula($conexao,$aluno_id,$calendario_ano){
 $res=$conexao->query("SELECT * FROM ecidade_matricula, turma where 
                     calendario_ano='$calendario_ano' and matricula_ativa='S'  
-                     and aluno_id=57248 and turma.idturma=ecidade_matricula.turma_id and turma.serie_id !=17 and turma.serie_id !=18;");
+                     and aluno_id=$aluno_id and turma.idturma=ecidade_matricula.turma_id and turma.serie_id !=17 and turma.serie_id !=18;");
 return $res->fetchAll();
 }
 
