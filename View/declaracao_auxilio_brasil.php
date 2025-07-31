4660b1a892341c12800e6be829828a3566e3880d
Arivan Amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-07-31 17:18:45 -0300

    declaracao bolsa ajust

--- View/declaracao_auxilio_brasil.php
+++ View/declaracao_auxilio_brasil.php
@@ -171,7 +171,7 @@ if ( ($seguimento!='' && $seguimento <3) || $idserie <8 ) {
                       ecidade_matricula.turma_id = turma.idturma and 
                       ecidade_matricula.turma_escola = escola.idescola and 
                       ecidade_matricula.turma_escola = $escola_id and 
-                      ecidade_matricula.matricula_situacao !='CANCELADO' 
+                      ecidade_matricula.matricula_situacao !='CANCELADO' AND matricula_ativa='S'
                       ORDER by ecidade_matricula.calendario_ano desc");
                        $nome_escola="";
                        $nome_turma="";
