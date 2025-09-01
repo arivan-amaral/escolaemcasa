3ebccb9c330aa7c497f0429ea2030303d3172126
Arivan Amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-09-01 15:30:35 -0300

    AJUSTES ACEITE DE TRANSFERENCIA

--- Model/Turma.php
+++ Model/Turma.php
@@ -217,7 +217,7 @@ function lista_de_turmas_das_escolas_rematricula($conexao,$serie_id,$escola_id,$
    turma.serie_id=serie.id and
    relacionamento_turma_escola.turma_id=turma.idturma and 
    relacionamento_turma_escola.escola_id=$escola_id and (
-   turma.serie_id=$serie_id OR turma.serie_id=16 )and
+   turma.serie_id=$serie_id OR turma.serie_id=15 OR turma.serie_id=16 )and
      turno='$turno' and
      ano=$ano_letivo_vigente 
 
