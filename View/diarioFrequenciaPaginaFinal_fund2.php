3196a8ea47dd06c2e5de1fe3b7a11c6d53b8a655
Arivan Amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-12-05 14:05:49 -0300

    AJUSTES

--- View/diarioFrequenciaPaginaFinal_fund2.php
+++ View/diarioFrequenciaPaginaFinal_fund2.php
@@ -400,6 +400,26 @@ function diario_frequencia_pagina_final_fund2($conexao,$idescola,$idturma,$iddis
                     line-height:normal'><div class="Namerotate" ><span style='font-size:10.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
                     mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Arial;
                     color:black;mso-fareast-language:PT-BR'>TRIM I<o:p></o:p></span></div></p>
+            </td>  
+
+
+            <td width=60 nowrap rowspan=2 style='width:12.0pt; border-top::solid windowtext 1.0pt; border-left:
+                solid windowtext 1.0pt;border-bottom:solid black 1.0pt;border-right:solid windowtext 1.0pt;
+                mso-rotate:90;height:12.0pt'>
+                <p class=MsoNormal align=center style='margin-bottom:0cm;text-align:center;
+                    line-height:normal'><div class="Namerotate" ><span style='font-size:10.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
+                    mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Arial;
+                    color:black;mso-fareast-language:PT-BR'>TRIM II<o:p></o:p></span></div></p>
+            </td>   
+
+            
+            <td width=60 nowrap rowspan=2 style='width:12.0pt; border-top::solid windowtext 1.0pt; border-left:
+                solid windowtext 1.0pt;border-bottom:solid black 1.0pt;border-right:solid windowtext 1.0pt;
+                mso-rotate:90;height:12.0pt'>
+                <p class=MsoNormal align=center style='margin-bottom:0cm;text-align:center;
+                    line-height:normal'><div class="Namerotate" ><span style='font-size:10.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
+                    mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Arial;
+                    color:black;mso-fareast-language:PT-BR'>TRIM III<o:p></o:p></span></div></p>
             </td>   
 
 
@@ -412,6 +432,10 @@ function diario_frequencia_pagina_final_fund2($conexao,$idescola,$idturma,$iddis
                     mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Arial;
                     color:black;mso-fareast-language:PT-BR'>FALTAS<o:p></o:p></span></div></p>
             </td>
+
+
+
+
         </tr>
 
         <tr style='mso-yfti-irow:11;height:58.75pt'>
