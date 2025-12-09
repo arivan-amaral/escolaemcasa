50039ddcd3830868e1b257204a9742ec1ea29c1f
Arivan Amaral <52377862+arivan-amaral@users.noreply.github.com>
2025-12-09 19:14:04 -0300

    ajustes

---
+++ View/ata_script_matricula.php
@@ -0,0 +1,233 @@
+<?php 
+session_start();
+
+
+$usuariobd=$_SESSION['usuariobd'];
+include_once "../Model/Conexao.php";
+include_once '../Controller/Conversao.php';
+include_once '../Model/Aluno.php';
+include_once '../Model/Turma.php';
+include_once '../Model/Nota.php';
+include_once 'ata_script_matricula_funcao.php';
+include_once '../Controller/Cauculos_notas.php';
+// include_once 'capa_turma_funcao.php';
+ 
+$idescola=$_GET['idescola'];
+$idturma=$_GET['idturma'];
+$nome_escola=$_GET['nome_escola'];
+$nome_turma=$_GET['nome_turma'];
+ 
+
+?>
+<html xmlns:v="urn:schemas-microsoft-com:vml"
+xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns:w="urn:schemas-microsoft-com:office:word"
+xmlns:x="urn:schemas-microsoft-com:office:excel"
+xmlns:m="http://schemas.microsoft.com/office/2004/12/omml"
+xmlns="http://www.w3.org/TR/REC-html40">
+
+<head>
+<meta charset="UTF-8">
+<meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+<meta name=ProgId content=Word.Document>
+<meta name=Generator content="Microsoft Word 15">
+<meta name=Originator content="Microsoft Word 15">
+<link rel=File-List href="regitro_conteudo_arquivos/filelist.xml">
+<link rel=Edit-Time-Data href="regitro_conteudo_arquivos/editdata.mso">
+
+<link rel=themeData href="regitro_conteudo_arquivos/themedata.thmx">
+<link rel=colorSchemeMapping
+href="regitro_conteudo_arquivos/colorschememapping.xml">
+
+
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+        @media print {
+          body {
+            background: none;
+            -ms-zoom: 1.665;
+          }
+          div.portrait, div.landscape {
+            margin-left: 100;
+          
+            padding: 0;
+            border: none;
+            background: none;
+            size: 4in 6in landscape;
+          }
+          div.landscape {
+            transform: rotate(270deg) translate(-220mm, 0);
+            transform-origin: 0 0;
+          }
+
+        }
+
+   
+
+        table { page-break-inside:auto }
+        tr    { page-break-inside:avoid; page-break-after:auto }
+        thead { display:table-header-group }
+        tfoot { display:table-footer-group }
+
+
+    @media print {
+
+        .pagebreak { page-break-before: always; } /* page-break-after works, as well */
+      }
+
+
+</style>
+<style type="text/css" media="print">
+    .page
+    {
+     -webkit-transform: rotate(-90deg); 
+     -moz-transform:rotate(-90deg);
+     filter:progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
+    }
+
+
+</style>
+</head>
+
+
+
+    <style>
+        .load {
+            width: 100px;
+            height: 100px;
+            position: absolute;
+            top: 30%;
+            left: 45%;
+            color: blue;
+        }
+    </style>
+
+
+
+ <body>
+ 
+ <div class="load"> <i class="fa fa-cog fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> </div>
+
+<div class="content-wrapper" style="min-height: 529px;background-color: white;">
+ <section class="content">
+    <div class="container-fluid">
+<br>
+
+
+
+ <!-- <H1> <font color='red'>PÁGINA EM MANUTENÇÃO</font> </H1><BR> -->
+
+ <div class=WordSection1>
+
+ <table class=MsoNormalTable cellspacing=0 cellpadding=0 
+  style='width: 100%;border: 1pt solid black;'>
+
+  <tr style='mso-yfti-irow:0;mso-yfti-firstrow:yes;height:15.75pt'>
+   <td width=83 nowrap rowspan=1 valign=top style='width:62.6pt;border:solid windowtext 1.0pt;
+   border-right:none;mso-border-top-alt:solid windowtext .5pt;mso-border-left-alt:
+   solid windowtext .5pt;mso-border-bottom-alt:solid windowtext .5pt;padding:
+   0cm 3.5pt 0cm 3.5pt;height:15.75pt'>
+   <p class=MsoNormal align=center style='margin-bottom:0cm;text-align:center;
+   line-height:normal'>
+
+   <span style='mso-ignore:vglayout'>
+   <table cellpadding=0 cellspacing=0 align=left>
+    <tr>
+     <td width=11 height=2></td>
+    </tr>
+    <tr>
+     <td></td>
+     <td>
+      <img width=60 height=75 src="imagens/logo.png"
+     v:shapes="Imagem_x0020_2"></td>
+    </tr>
+   </table>
+
+   </span>
+   <span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+   "Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+   color:black;mso-fareast-language:PT-BR'><o:p>&nbsp;</o:p></span></p>
+   </td>
+
+   <td  colspan="6" valign=top style='border:1pt;border:
+   solid windowtext 1.0pt;mso-border-top-alt:solid windowtext .5pt;padding:10pt 3.5pt 0cm 3.5pt;
+   height:15.75pt; text-align: center;'>
+   <p class=MsoNormal style='margin-bottom:9pt;line-height:normal'><b><span
+   style='font-size:12.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
+   mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
+   color:black;mso-fareast-language:PT-BR'><?php echo $_SESSION['ORGAO']; ?><o:p></o:p></span></b></p>
+
+     <p class=MsoNormal style='margin-bottom:0cm;line-height:normal'><b><span
+     style='font-size:12.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
+     mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
+     color:black;mso-fareast-language:PT-BR'><?php echo "$nome_escola"; ?><o:p></o:p></span></b></p>
+     <p class=MsoNormal style='margin-bottom:0cm;line-height:normal'><b><span
+     style='font-size:12.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
+     mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
+     color:black;mso-fareast-language:PT-BR'>TURMA: <?php echo "$nome_turma"; ?><o:p></o:p></span></b></p>
+ 
+   <br>
+   </td>
+
+    <td  colspan="100%" valign=top style='background-color: #E0E0E0; border:1pt solid black; padding:10pt 3.5pt 0cm 3.5pt;
+   height:15.75pt; text-align: center;'>
+   <p class=MsoNormal style='margin-bottom:9pt;line-height:normal'><b><span
+   style='font-size:12.0pt;font-family:"Tw Cen MT Condensed",sans-serif;
+   mso-fareast-font-family:"Times New Roman";mso-bidi-font-family:Calibri;
+   color:black;mso-fareast-language:PT-BR'>Relatório de ATA final <?php echo $_SESSION['ano_letivo']; ?><o:p></o:p></span></b></p>
+    </td>
+  </tr>
+
+<?php 
+                                      
+$idescola=$_GET['idescola'];
+$idturma=$_GET['idturma'];
+$idserie=$_GET['idserie'];
+
+  //capa_turma($conexao,$idescola,$idturma);
+ ata_resultados_finais($conexao,$idescola,$idturma,$idserie,$_SESSION['ano_letivo']);
+?>
+</table>
+
+</div>
+<br>
+<script>
+    //alert("Estamos em manutenção na ATA");
+</script>
+
+<center>
+<footer class="no-print">
+  <i class="fa fa-print"></i>
+  <a href='#'class="btn  btn-primary " onclick='print();'>IMPRIMIR</a> <br><br>
+</footer>
+</center>
+    </div>
+  </session>
+</div>
+
+
+ <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+    <script>
+        //código usando jQuery
+        $( document ).ready(function() {
+            $('.load').hide();
+        });
+    </script>
+    
+</body>
+</html>
---
+++ View/ata_script_matricula_funcao.php
@@ -0,0 +1,515 @@
+<?php 
+function ata_resultados_finais($conexao,$idescola,$idturma,$idserie,$ano_letivo){
+
+?>
+
+ 
+ <tr style='width:100%'>
+ 
+  <td width=19 valign=top style='width:14.15pt;border:solid black 1.0pt;
+  padding:0cm 0cm 0cm 0cm;height:21.9pt'>
+  <p class=TableParagraph style='margin-top:0cm'><span lang=PT
+  style='font-size:8.0pt;font-family:"Times New Roman",serif'>&nbsp;</span></p>
+  </td>
+  <td width=246 valign=top style='width:184.25pt;border:solid black 1.0pt;
+  border-left:none;padding:0cm 0cm 0cm 0cm;height:21.9pt'>
+  <p class=TableParagraph align=right style='margin-top:.8pt;margin-right:2.05pt;
+  margin-bottom:0cm;margin-left:0cm;margin-bottom:.0001pt;text-align:right'><b><span
+  lang=PT style='font-size:7.0pt;font-family:"Arial",sans-serif'>Disciplinas</span></b></p>
+  <p class=TableParagraph align=right style='margin-top:3.3pt;margin-right:
+  2.0pt;margin-bottom:0cm;margin-left:0cm;margin-bottom:.0001pt;text-align:
+  right'><b><span lang=PT style='font-size:7.0pt;font-family:"Arial",sans-serif'>Carga
+  Horária</span></b></p>
+  </td>
+
+ <?php 
+  $res_disc=listar_disciplina_para_ata($conexao,$idescola,$idturma,$ano_letivo);
+  $conta_disciplina=0;
+  $array_disciplina=array();
+  $array_nome_sigla_disciplina=array();
+  $carga_horaria='';
+  foreach ($res_disc as $key => $value) {
+    $iddisciplina=$value['iddisciplina'];
+    $carga_horaria=$value['carga_horaria'];
+    
+    if ($idserie > 7 && $idserie <12 && $value['nome_disciplina']=='CIÊNCIAS') {
+      $nome_disciplina="Ciências Físicas e Biológicas";
+      $abreviacao="CFB";
+    }else{
+
+    $nome_disciplina=$value['nome_disciplina'];
+    $abreviacao=$value['abreviacao'];
+    }
+
+
+    $array_disciplina[$conta_disciplina]=$iddisciplina;
+    $array_nome_sigla_disciplina[$abreviacao]=$nome_disciplina;
+    $conta_disciplina++;
+?>
+  <td width=42 valign=top style='width:31.15pt;border:solid black 1.0pt;
+  border-left:none;padding:0cm 0cm 0cm 0cm;height:21.9pt'>
+  <p class=TableParagraph style='margin-top:.8pt;margin-right:0cm;margin-bottom:
+  0cm;margin-left:5.75pt;margin-bottom:.0001pt'><b><span lang=PT
+  style='font-size:7.0pt;font-family:"Arial",sans-serif'><?php echo strtoupper($abreviacao); ?></span></b></p>
+  <p class=TableParagraph style='margin-top:3.3pt;margin-right:0cm;margin-bottom:
+  0cm;margin-left:9.65pt;margin-bottom:.0001pt'><b><span lang=PT
+
+  style='font-size:7.0pt;font-family:"Arial",sans-serif'><?php echo $carga_horaria; ?></span></b></p>
+  </td>
+
+  <!--  <td width=42 valign=top style='width:31.15pt;border:solid black 1.0pt;
+  border-left:none;padding:0cm 0cm 0cm 0cm;height:21.9pt'>
+  <p class=TableParagraph style='margin-top:.8pt;margin-right:0cm;margin-bottom:
+  0cm;margin-left:5.75pt;margin-bottom:.0001pt'><b><span lang=PT
+  style='font-size:7.0pt;font-family:"Arial",sans-serif'>mnbnbh<?php echo strtoupper($abreviacao); ?></span></b></p>
+  <p class=TableParagraph style='margin-top:3.3pt;margin-right:0cm;margin-bottom:
+  0cm;margin-left:9.65pt;margin-bottom:.0001pt'><b><span lang=PT
+
+  style='font-size:7.0pt;font-family:"Arial",sans-serif'>160</span></b></p>
+  </td>
+   -->
+<?php 
+
+}
+?>
+
+ 
+  
+  <td  style='border:solid black 1.0pt;
+  border-left:none;padding:0cm 0cm 0cm 0cm;height:21.9pt'>
+  <p class=TableParagraph align=center style='margin-top:.8pt;margin-right:
+  10.25pt;margin-bottom:0cm;margin-left:11.6pt;margin-bottom:.0001pt;
+  text-align:center'><b><span lang=PT style='font-size:7.0pt;font-family:"Arial",sans-serif'>RF</span></b></p>
+  </td>
+ </tr>
+
+ <tr >
+  <td width=19  valign=top style='width:14.15pt;border-top:none;border-left:
+  solid black 1.0pt;border-bottom:none;border-right:solid black 1.0pt;
+  padding:0cm 0cm 0cm 0cm;height:10.95pt'>
+  <p class=TableParagraph style='margin-top:.8pt;margin-right:0cm;margin-bottom:
+  0cm;margin-left:3.15pt;margin-bottom:.0001pt'><b><span lang=PT
+  style='font-size:7.0pt;font-family:"Arial",sans-serif'>Nº</span></b></p>
+  </td>
+  <td colspan="<?php echo $conta_disciplina+2; ?>"  style='border:none;border:solid black 1.0pt;
+  padding:0cm 0cm 0cm 0cm;height:10.95pt'>
+      <p class=TableParagraph  style='margin-top:.8pt;margin-right:
+      64.55pt;margin-bottom:0cm;margin-left:65.15pt;margin-bottom:.0001pt;
+       '><b><span lang=PT style='font-size:7.0pt;font-family:"Arial",sans-serif'>Nome
+      do Aluno.</span></b></p>
+  </td>
+
+<?php
+$conta_aluno=1; 
+$matricula_aluno="";
+ 
+if ($_SESSION['ano_letivo']==$_SESSION['ano_letivo_vigente']) {
+  $res_alunos=listar_aluno_da_turma_ata_resultado_final($conexao,$idturma,$idescola,$_SESSION['ano_letivo']);
+}else{
+  $res_alunos=listar_aluno_da_turma_ata_resultado_final_matricula_concluida($conexao,$idturma,$idescola,$_SESSION['ano_letivo']);
+ }
+
+
+ foreach ($res_alunos as $key => $value) {
+
+  $idaluno=$value['idaluno'];
+  $nome_aluno=($value['nome_aluno']);
+  $matricula_aluno=$value['matricula'];
+
+  if ($conta_aluno%2==0) {
+    $cor_linha="#E0E0E0";
+  }else{
+    $cor_linha="white";
+
+  }
+
+// pesquisar_aluno_da_turma_ata_resultado_final
+  $res_movimentacao=pesquisar_aluno_da_turma_ata_resultado_final($conexao,$matricula_aluno,$_SESSION['ano_letivo']);
+
+  $data_evento="";
+  $descricao_procedimento="";
+  $procedimento="";
+  $matricula="";
+  foreach ($res_movimentacao as $key => $value) {
+      $datasaida=($value['datasaida']);
+      
+      $procedimento=$value['procedimento'];
+      
+      if ($datasaida!="") {
+        $datasaida=converte_data($datasaida);
+      }
+  }
+
+?>
+ <tr style=' '>
+ 
+  <?php 
+ 
+  if ($procedimento!='') {
+  
+  ?>
+      <td width=19 valign=top style='background-color: <?php echo "$cor_linha"; ?>; width:14.15pt;border:solid black 1.0pt;padding:0cm 0cm 0cm 0cm;height:11.3pt'>
+
+      <p class=TableParagraph style='margin-top:1.8pt;margin-right:0cm;margin-bottom:
+      0cm;margin-left:3.65pt;margin-bottom:.0001pt'>
+      <span lang=PT style='font-size:
+      8.0pt'><?php echo "$conta_aluno"; ?>
+    </span></p>
+      </td>
+
+      <td  valign=top style='border:solid black 1.0pt; 
+      padding:0cm 0cm 0cm 0cm;height:11.3pt;background-color: <?php echo "$cor_linha"; ?>;'>
+      <p class=TableParagraph style='margin-top:1.8pt;margin-right:0cm;margin-bottom:
+      0cm;margin-left:2.75pt;margin-bottom:.0001pt'><span lang=PT style='font-size:
+      8.0pt; text-transform: uppercase;'><?php echo "$nome_aluno"; ?></span></p>
+      </td>
+
+
+     <td colspan='100%' valign=top style='border:solid black 1.0pt;
+     ;padding:0cm 0cm 10pt 0cm;height:11.3pt;background-color: <?php echo "$cor_linha"; ?>;'>
+      <p class=TableParagraph style='margin-top:1.8pt;margin-right:0cm;margin-bottom:
+      0cm;margin-left:2.75pt;margin-bottom:.0001pt'><span lang=PT style='font-size:
+      8.0pt'><?php echo" $procedimento  $datasaida "; ?></p>
+      </td> 
+
+  <?php
+    $conta_aluno++; 
+   }else{
+  ?>
+  <td width=19 valign=top style='width:14.15pt;border:solid black 1.0pt;  background:<?php echo "$cor_linha"; ?>;padding:0cm 0cm 0cm 0cm; '>
+
+  <p class=TableParagraph style='margin-left:5.3pt'><span lang=PT
+  style='font-size:8.0pt'><?php echo "$conta_aluno"; ?></span></p>
+  </td>
+
+  <td width=246 valign=top style='width:184.25pt;border:solid black 1.0pt;
+  background:<?php echo "$cor_linha"; ?>;padding:0cm 0cm 0cm 0cm;'>
+  <p class=TableParagraph style='margin-left:2.75pt'><span lang=PT
+  style='font-size:8.0pt; text-transform: uppercase;'><?php echo $nome_aluno; ?></span></p>
+  </td>
+  
+<?php
+
+      $iddisciplina="";
+      $media_aprovacao="Não";
+      $aprovacao_conselho="Não";
+      $conta_apc=0;
+      $conta_apr=0;
+      foreach ($array_disciplina as $key => $value) {
+           $media_aprovacao="Não";
+           $aprovacao_conselho="Não";
+            
+            $iddisciplina=$array_disciplina[$key];
+         
+  ?>
+          <td width=42 valign=top style='width:31.15pt;border:solid black 1.0pt;
+          background:<?php echo "$cor_linha"; ?>;padding:0cm 0cm 0cm 0cm;'>
+          <p class=TableParagraph align=center style='margin-right:
+          2.7pt;margin-bottom:0cm;margin-left:3.35pt;text-align:
+          center'><span lang=PT style='font-size:8.0pt'>
+        <?php
+        if ($idserie>3 && $_SESSION['nivel_acesso_id']==1011) {
+           $media=gerar_media_ata($conexao,$idescola,$idturma,$iddisciplina,$idaluno,$ano_letivo,$idserie);
+          echo "Manutenção:$media <br>";
+
+        }elseif ($idserie>3) {
+  
+             $result_nota_aula1=pesquisa_nota_por_periodo($conexao,$idescola,$idturma,$iddisciplina,$idaluno,1,$ano_letivo);
+
+             $nota_tri_1=0;
+             $nota_av3_1='';
+             $nota_rp_1='';
+             foreach ($result_nota_aula1 as $key => $value) {
+               if ($value['avaliacao']!='RP') {
+                 $nota_tri_1=$nota_tri_1+$value['nota'];
+               }
+            // ***************************************
+               if ($value['avaliacao']=='av3') {
+                 $nota_av3_1=$value['nota'];
+               }
+               if ($value['avaliacao']=='RP') {
+                 $nota_rp_1=$value['nota'];
+               }
+             }
+        
+      $nota_tri_1=calculos_media_notas($nota_tri_1,$nota_rp_1,$nota_av3_1);
+
+            //echo "$nota_tri_1";
+    
+  
+      $result_nota_aula2=pesquisa_nota_por_periodo($conexao,$idescola,$idturma,$iddisciplina,$idaluno,2,$ano_letivo);
+ 
+      $nota_tri_2=0;
+      $nota_av3_2='';
+      $nota_rp_2='';
+      foreach ($result_nota_aula2 as $key => $value) {
+
+        if ($value['avaliacao']!='RP') {
+          $nota_tri_2=$nota_tri_2+$value['nota'];
+
+
+        }
+          // ***************************************
+        if ($value['avaliacao']=='av3') {
+          $nota_av3_2=$value['nota'];
+
+        }
+
+        if ($value['avaliacao']=='RP') {
+          $nota_rp_2=$value['nota'];
+
+
+        }
+
+      }
+
+    
+      $nota_tri_2=calculos_media_notas($nota_tri_2,$nota_rp_2,$nota_av3_2);
+     
+
+    // echo "$nota_tri_2";
+ 
+
+   $result_nota_aula3=pesquisa_nota_por_periodo($conexao,$idescola,$idturma,$iddisciplina,$idaluno,3,$ano_letivo);
+
+
+   $nota_tri_3=0;
+   $nota_av3_3='';
+   $nota_rp_3=0;
+   foreach ($result_nota_aula3 as $key => $value) {
+
+         if ($value['avaliacao']!='RP') {
+           $nota_tri_3=$nota_tri_3+$value['nota'];
+         }
+           // ***************************************
+         if ($value['avaliacao']=='av3') {
+           $nota_av3_3=$value['nota'];
+         }
+
+         if ($value['avaliacao']=="RP") {
+           $nota_rp_3=$value['nota'];
+         }
+
+   }
+
+  $nota_tri_3=calculos_media_notas($nota_tri_3,$nota_rp_3,$nota_av3_3);
+ 
+  
+  if ($_SESSION['nivel_acesso_id']>=1010) {
+  
+   echo " <br>{ | t1: $nota_tri_1 | t2: $nota_tri_2  | t3: $nota_tri_3 ($nota_tri_3,$nota_rp_3,$nota_av3_3) }<br> ";
+
+  }
+
+
+  $res_fora=pesquisa_nota_fora_rede($conexao,$idescola,$idturma,$iddisciplina,$idaluno,7,$ano_letivo,$idserie);
+
+
+  $media_fora_rede=0;
+  foreach ($res_fora as $key_fora => $value_f) {
+    $media_fora_rede+=$value_f['nota'];
+  }
+
+
+
+
+if ($media_fora_rede==0) {
+  // $nota_tri_3=calculos_media_notas($nota_tri_3,$nota_rp_3,$nota_av3_3);
+   $media=($nota_tri_1+$nota_tri_2+$nota_tri_3)/3;
+}else{
+ // $media=( $media_fora_rede +$nota_tri_2+$nota_tri_3)/3;
+}
+ //arivan
+  $media=number_format($media, 1, '.', ',');
+
+  // arivan removeu devido problema de lentidão 02/12/2025  
+  // 
+  // 
+  // 
+  // 
+  // 
+  // if ($_SESSION['nivel_acesso_id']>=1020) {
+
+  //   $res_hist=$conexao->query("
+  //     SELECT * from historico where 
+  //     aluno_id =$idaluno and 
+  //     disciplina_id = $iddisciplina and
+  //     serie_id= $idserie LIMIT 1
+   
+  //   ");
+  //  // $res_hist->execute();
+
+  //  $quantidade_hist=0;
+  //  foreach ($res_hist as $key_h => $value_h) {
+  //     $quantidade_hist++;
+     
+  //  }
+  //   if ($quantidade_hist==0) {
+  //    //echo "(:". $res_hist->rowCount() .":";
+  //      $conexao->exec("
+  //         INSERT INTO historico (aluno_id,ano, nota_final, disciplina_id, serie_id, escola_id)
+  //       VALUES ($idaluno,$ano_letivo, $media, $iddisciplina, $idserie, $idescola)
+  //       ");
+
+  //   }else{
+  //    //echo "==". $res_hist->rowCount() .":";
+
+  //     $conexao->exec("
+  //       UPDATE historico SET 
+  //       aluno_id=$idaluno,
+  //       ano=$ano_letivo,
+  //       nota_final=$media,
+  //       disciplina_id=$iddisciplina,
+  //       serie_id=$idserie, 
+  //       escola_id=$idescola
+  //       where
+  //         aluno_id =$idaluno and 
+        
+  //         disciplina_id = $iddisciplina and
+  //         serie_id= $idserie
+  //     ");
+  //   }
+
+   
+  // }
+
+
+
+  if ($media >= 5) {
+      $conta_apr++;
+      $media_aprovacao="Apr";
+      $aprovacao_conselho="Não";
+      echo  number_format($media, 1, '.', ',');
+
+  }else{
+      $res_conselho=buscar_aprovar_concelho($conexao,$idescola,$idturma,$iddisciplina,$idaluno,$ano_letivo);
+      $conta_aprovado=count($res_conselho);
+      
+       if ($conta_aprovado>0 ) {
+          $media_conselho=number_format('5', 1, '.', ',');
+          echo "<b>$media_conselho</b>";
+          
+          $media_aprovacao="Não";
+          $aprovacao_conselho="Apc";
+          $conta_apc++;
+
+      }else{
+          $media_aprovacao="Não";
+
+          echo "". number_format($media, 1, '.', ',') ."";
+
+      }
+
+  }
+
+}//se serie for menor que 3
+else{
+  echo "Apr";
+  $media_aprovacao="Apr";
+
+
+}
+?>
+      </span></p>
+          </td>
+<?php 
+  }
+?>
+  <td width=45 valign=top style='width:34.0pt;border: solid black 1.0pt;
+  background:<?php echo "$cor_linha"; ?>;padding:0cm 0cm 0cm 0cm;'>
+  <p class=TableParagraph align=center style='margin-top:1.85pt;margin-right:
+  10.25pt;margin-bottom:0cm;margin-left:11.6pt;
+  text-align:center'><span lang=PT style='font-size:8.0pt'>
+<?php 
+$total_conta_apc=$conta_apr+$conta_apc;
+
+if ($_SESSION['idfuncionario']==176) {
+  //echo "$total_conta_apc==".count($array_disciplina) ."&& $conta_apc>0";
+}
+
+    if($idserie<=3){
+        echo "<b style='color: green;'>Apr</b>";
+
+   }
+    elseif (  $total_conta_apc==count($array_disciplina) && $conta_apc>0) {
+         echo "<b style='color: blue;'>Apc </b> ";
+    }
+    elseif ($media_aprovacao == "Apr" && $total_conta_apc==count($array_disciplina)  ) {
+         echo "<b style='color: green;'>Apr</b>";
+    }elseif ($media_aprovacao == "Não"){
+      $media_aprovacao="Não";
+         echo "<b style='color: red;'>Rep</b>";
+
+    }elseif ($aprovacao_conselho == "Não"){
+      $media_aprovacao="Não";
+         echo "<b style='color: red;'>Rep</b>";
+
+    }
+?>
+
+
+</span></p>
+  </td>
+ </tr>
+
+<?php 
+$conta_aluno++; 
+
+}
+
+}//else se não houve movimentação escolar
+?>
+
+ <tr style='height:10.55pt'>
+  <td width=321 colspan=4 valign=top style='border:solid black 1.0pt;
+  padding:0pt 0pt 10pt 0pt;height:10.55pt'>
+  <p class=TableParagraph align=center style='margin-top:1.45pt;margin-left:1.85pt;margin-bottom:5pt;
+  text-align:center'><span lang=PT style='font-size:10.0pt'>Assinatura dos professores:</span></p>
+  </td>  
+
+  <td width=321 colspan=10 valign=top style='border:solid black 1.0pt;
+  padding:0pt 0pt 10pt 0pt;height:10.55pt'>
+  <p class=TableParagraph align=center style='margin-top:1.45pt;margin-left:4.85pt;margin-bottom:5pt;
+  text-align:center'><span lang=PT style='font-size:10.0pt'>Convenções:</span>
+</p>
+ <p style='margin-top:1.45pt;margin-left:4.85pt;margin-bottom:5pt;padding:0pt 0pt 10pt 5pt;'> 
+  <?php 
+    $conta_dis=1;
+    foreach ($array_nome_sigla_disciplina as $key => $value) {
+      echo "<span lang=PT style='font-size:8.0pt'> $key - $value &nbsp;&nbsp;&nbsp;</span>";
+      if ($conta_dis%2==0) {
+       echo "<br>";
+      }
+      $conta_dis++;
+    }
+    echo "<span lang=PT style='font-size:8.0pt'> APC -Aprovado pelo conselho &nbsp;&nbsp;  </span>";
+    echo "<span lang=PT style='font-size:8.0pt'> APR -Aprovado &nbsp;&nbsp;</span>";
+    echo "<span lang=PT style='font-size:8.0pt'> REP -REPROVADO &nbsp;&nbsp;</span>";
+
+  ?>
+</p>
+
+  <p  align=center style='border-top: 1px solid black; margin-top:1.45pt;margin-left:0.85pt;margin-bottom:5pt;
+  text-align:center'></p>  
+  <p  align=center style=' font-size:8.0pt margin-top:1.45pt;margin-left:0.85pt;margin-bottom:5pt;
+  text-align:center'>
+
+  E, para constar, foi lavrada esta Ata. <br>
+  ___________________________________________<br>
+<?php echo $_SESSION['CIDADE']; ?>, <?php echo date("d/m/Y"); ?>
+</p> 
+  </td>
+  <!-- <td width=404 colspan=10  style='border:solid black 1.0pt;
+  border-left:none;padding:100pt;height:10.55pt'>
+  <p class=TableParagraph align=center style='margin-top:1.45pt;margin-left:1.85pt;margin-bottom:.0001pt;
+  text-align:center'>Convenções:</span></p>
+  </td> -->
+ </tr>
+ 
+  
+
+
+<?php 
+}
+
+?>
